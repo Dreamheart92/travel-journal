9478e65737ed9dd9d95458c674ffe177ea4417f5
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 18:47:37 +0300

    created input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import ErrorMessage from '../ErrorMessage';
+
+import { normalizeName } from '../../utility/utility';
+
+import style from './index.module.css';
+
+export default function Input({ label, children, ...props }) {
+  const displayError = props.state.error && (props.state.isDirty
+    || props.formState.isSubmittedAndHasErrors);
+
+  return (
+    <>
+      {label
+        && <label htmlFor={label}>{normalizeName(label)}</label>}
+
+      <div className={style.wrapper}>
+        {children}
+        {displayError
+          && <ErrorMessage message={Object.entries(props.state.error)[0][1]} />}
+      </div>
+    </>
+  );
+}
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  props: PropTypes.object.isRequired,
+};
