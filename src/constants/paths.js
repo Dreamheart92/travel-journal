3ad0cbb1efcf842fb734e6bc2b4c1470f7fe819c
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 12:44:07 +0300

    created paths constants

---
+++ src/constants/paths.js
@@ -0,0 +1,14 @@
+export const PATHS = {
+  HOME: '/',
+  CATALOGUE: '/journals',
+  DETAILS: '/journal/:id',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  CREATE: '/create',
+  ACCOUNT: '/account',
+};
+
+export const ACCOUNT_SUB_PATHS = {
+  EDIT_PROFILE: `${PATHS.ACCOUNT}/edit-profile`,
+  MY_JOURNALS: `${PATHS.ACCOUNT}/my-journals`,
+};
