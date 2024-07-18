088a9ff083761da66e295df4404811cb6f3c287e
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 22:54:54 +0300

    refactored

--- src/components/Input/TextInput/index.jsx
+++ src/components/Input/TextInput/index.jsx
@@ -1,17 +1,43 @@
-import Input from '../';
+import PropTypes from 'prop-types';
+
+import Wrapper from '..';
 
 import style from '../index.module.css';
+import { handlersPropType, isSubmittedAndHasErrorsPropType, statePropType } from '../../../propTypes/inputPropTypes';
 
-export default function TextInput({ label, type, children, ...props }) {
-    const handlers = props.handlers;
-
-    const inputType = type === "password" ? type : "text";
-
-    return (
-        <Input label={label} {...props}>
-            <input className={style.field} {...handlers} placeholder={children}
-                type={inputType}
-                value={props.state.value} />
-        </Input>
-    )
+export default function TextInput(
+  {
+    label,
+    type = 'text',
+    children,
+    handlers,
+    state,
+    isSubmittedAndHasErrors,
+  },
+) {
+  return (
+    <Wrapper
+      state={state}
+      label={label}
+      isSubmittedAndHasErrors={isSubmittedAndHasErrors}
+    >
+      <input
+        className={style.field}
+        onChange={handlers.onChange}
+        onBlur={handlers.onBlur}
+        placeholder={children}
+        type={type}
+        value={state.value}
+      />
+    </Wrapper>
+  );
 }
+
+TextInput.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  children: PropTypes.string,
+  handlers: handlersPropType,
+  state: statePropType,
+  isSubmittedAndHasErrors: isSubmittedAndHasErrorsPropType,
+};
