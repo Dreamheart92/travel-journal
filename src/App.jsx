ec499ccc1f753eba64c7aaecc8f74a65d93e19cf
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 17:41:25 +0300

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
+import Navigation from './components/Navigation';
 
 export default function App() {
   return (
     <DefaultLayout>
+      <Navigation />
       <Outlet />
     </DefaultLayout>
   );
