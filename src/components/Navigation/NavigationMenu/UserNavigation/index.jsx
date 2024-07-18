502aace72fb7b30681d6e863eef93dc0628c0da6
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 20:21:18 +0300

    passing onLogout

--- src/components/Navigation/NavigationMenu/UserNavigation/index.jsx
+++ src/components/Navigation/NavigationMenu/UserNavigation/index.jsx
@@ -1,12 +1,12 @@
 import NavigationLink from '../../NavigationLink';
 import { PATHS } from '../../../../constants/paths';
 
-export default function UserNavigation() {
+export default function UserNavigation({ onLogout }) {
   return (
     <>
       <NavigationLink to={PATHS.CREATE}>Create</NavigationLink>
       <NavigationLink to={PATHS.ACCOUNT}>Account</NavigationLink>
-      <NavigationLink to="">Logout</NavigationLink>
+      <NavigationLink onClick={onLogout} to="">Logout</NavigationLink>
     </>
   );
 }
