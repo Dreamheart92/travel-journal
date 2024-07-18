3ab257cb6a7fb7043e3820f3d4c7d324d253f3ae
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 16:24:02 +0300

    created default layout

---
+++ src/layouts/DefaultLayout/index.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import style from './index.module.css';
+
+export default function DefaultLayout({ children }) {
+  return (
+    <div className={style.container}>
+      {children}
+    </div>
+  );
+}
+
+DefaultLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
