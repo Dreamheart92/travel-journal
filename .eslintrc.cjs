d0df567991b96c3281088d7f15d0acc54972b498
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 22:54:12 +0300

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'no-underscore-dangle': ['error', { 'allow': ['_id'] }],
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
