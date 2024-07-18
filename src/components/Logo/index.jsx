9bb5c620e045a91e912a547385c3b3b4cd9b3623
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 16:30:05 +0300

    created logo component

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import { PATHS } from '../../constants/paths';
+
+import style from './index.module.css';
+
+export default function Logo() {
+  return (
+    <div className={style.logo}>
+      <Link to={PATHS.HOME}>
+        <h1>TRAVEL JOURNAL</h1>
+      </Link>
+    </div>
+  );
+}
