6ec225ae3406b660bc52efbf1bd4fb177ad0ab3e
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 18:32:40 +0300

    created info block component

---
+++ src/components/InfoBlock/index.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+import style from './index.module.css';
+
+export default function InfoBlock({ title, caption }) {
+  return (
+    <div className={style['info-block']}>
+      <h1>{title}</h1>
+      <p>{caption}</p>
+    </div>
+  );
+}
+
+InfoBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+};
