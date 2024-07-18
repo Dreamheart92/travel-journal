9b920f7d756577d4718de90685f26e58b7f2402d
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 17:42:20 +0300

    created guest navigation

---
+++ src/components/Navigation/NavigationMenu/GuestNavigation/index.jsx
@@ -0,0 +1,11 @@
+import NavigationLink from '../../NavigationLink';
+import { PATHS } from '../../../../constants/paths';
+
+export default function GuestNavigation() {
+  return (
+    <>
+      <NavigationLink to={PATHS.LOGIN}>Login</NavigationLink>
+      <NavigationLink to={PATHS.SIGNUP}>Signup</NavigationLink>
+    </>
+  );
+}
