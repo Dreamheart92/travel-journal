22a62650b8c3a8cd32bc8f2db4a6f24ceeb22eed
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 13:00:37 +0300

    created generic send http request func

---
+++ src/services/sendHttpRequest.js
@@ -0,0 +1,25 @@
+const sentHttpRequest = async (url, settings) => {
+  try {
+    const response = await fetch(url, settings);
+
+    if (response.status === 404) {
+      return window.location.replace('404');
+    }
+
+    if (response.status === 204) {
+      return response;
+    }
+
+    const result = await response.json();
+
+    if (result.success) {
+      return result;
+    }
+
+    throw result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default sentHttpRequest;
