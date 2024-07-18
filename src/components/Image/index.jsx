9b9cb3cca0366cb6747b22a65d9ae0835fe1a803
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 16:23:50 +0300

    created image component

---
+++ src/components/Image/index.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export default function Image({ imageUrl }) {
+  return (
+    <img src={imageUrl} alt="" />
+  );
+}
+
+Image.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+};
