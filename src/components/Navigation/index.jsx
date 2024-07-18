7b5cbe1e98a9b7760150064752db97d23d311559
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 20:20:47 +0300

    created handle logout function

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import NavigationLayout from '../../layouts/NavigationLayout';
 import Logo from './Logo';
 import NavigationMenu from './NavigationMenu';
+import { deleteUserDataFromStorage } from '../../utility/storage';
+import { PATHS } from '../../constants/paths';
 
 export default function Navigation() {
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+    deleteUserDataFromStorage();
+    navigate(PATHS.HOME);
+  };
+
   return (
     <NavigationLayout>
       <Logo />
-      <NavigationMenu />
+      <NavigationMenu onLogout={handleLogout} />
     </NavigationLayout>
   );
 }
