6f7af8c4a0c2e7a7e4bdd7b750a2eaa367e4468a
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 22:54:48 +0300

    refactored

--- src/components/Input/TextAreaInput/index.jsx
+++ src/components/Input/TextAreaInput/index.jsx
@@ -1,15 +1,41 @@
-import Input from "../Input.jsx";
+import PropTypes from 'prop-types';
+import { statePropType, isSubmittedAndHasErrorsPropType, handlersPropType } from '../../../propTypes/inputPropTypes';
 
-import style from "../index.module.css";
+import Wrapper from '..';
+import style from '../index.module.css';
 
-export default function TextAreaInput({ label, children, ...props }) {
-    const handlers = props.handlers;
-
-    return (
-        <Input label={label} {...props}>
-            <textarea className={style.field} {...handlers}
-                value={props.state.value} placeholder={children} rows="10"
-                cols="50"></textarea>
-        </Input>
-    )
+export default function TextAreaInput(
+  {
+    label,
+    children,
+    handlers,
+    state,
+    isSubmittedAndHasErrors,
+  },
+) {
+  return (
+    <Wrapper
+      label={label}
+      state={state}
+      isSubmittedAndHasErrors={isSubmittedAndHasErrors}
+    >
+      <textarea
+        className={style.field}
+        onChange={handlers.onChange}
+        onBlur={handlers.onBlur}
+        value={state.value}
+        placeholder={children}
+        rows="10"
+        cols="50"
+      />
+    </Wrapper>
+  );
 }
+
+TextAreaInput.propTypes = {
+  label: PropTypes.string,
+  children: PropTypes.string,
+  handlers: handlersPropType,
+  state: statePropType,
+  isSubmittedAndHasErrors: isSubmittedAndHasErrorsPropType,
+};
