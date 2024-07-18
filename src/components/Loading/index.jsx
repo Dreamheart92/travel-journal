393c9f3d2750d138851dcd3331b25d855db4ff32
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 20:20:28 +0300

    created loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import style from './index.module.css';
+
+import loading from "/loading.jpg"
+
+export default function Loading() {
+  return (
+    <div className={style.wrapper}>
+      <img src={loading} alt="" />
+    </div>
+  );
+}
