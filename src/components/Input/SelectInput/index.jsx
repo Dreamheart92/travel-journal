fd2416cbd1dca2762a258b29bdb607e1672b779a
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 22:54:44 +0300

    refactored

--- src/components/Input/SelectInput/index.jsx
+++ src/components/Input/SelectInput/index.jsx
@@ -1,20 +1,51 @@
-import { normalizeName } from '../../../../utility/utility.js';
+import PropTypes from 'prop-types';
+import { handlersPropType, statePropType, isSubmittedAndHasErrorsPropType } from '../../../propTypes/inputPropTypes';
+import { normalizeName } from '../../../utility/utility';
 
-import Input from '../Input.jsx';
+import Wrapper from '..';
 
 import style from '../index.module.css';
 
-export default function SelectInput({ options, ...props }) {
-    const handlers = props.handlers;
-
-    return (
-        <Input {...props}>
-            <select className={style.field} value={props.state.value} {...handlers} name="destination">
-                <option value="" disabled>Select a destination</option>
-                {options.map(option => (
-                    <option key={option._id} value={option._id}>{normalizeName(option.name)}</option>
-                ))}
-            </select>
-        </Input>
-    )
+export default function SelectInput(
+  {
+    options,
+    handlers,
+    state,
+    isSubmittedAndHasErrors,
+  },
+) {
+  return (
+    <Wrapper
+      state={state}
+      isSubmittedAndHasErrors={isSubmittedAndHasErrors}
+    >
+      <select
+        className={style.field}
+        value={state.value}
+        onChange={handlers.onChange}
+        onBlur={handlers.onBlur}
+        name="destination"
+      >
+        <option value="" disabled>Select a destination</option>
+        {options.map((option) => (
+          <option
+            key={option._id}
+            value={option._id}
+          >
+            {normalizeName(option.name)}
+          </option>
+        ))}
+      </select>
+    </Wrapper>
+  );
 }
+
+SelectInput.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.shape([{
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }])),
+  handlers: handlersPropType,
+  state: statePropType,
+  isSubmittedAndHasErrors: isSubmittedAndHasErrorsPropType,
+};
