fdb442821b0851f5782f4856bc969784eee50a06
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 19:40:18 +0300

    added padding top

--- src/layouts/AuthLayout/index.jsx
+++ src/layouts/AuthLayout/index.jsx
@@ -1,16 +1,34 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+
 import InfoBlock from '../../components/InfoBlock';
+import Image from '../../components/Image';
+
+import authBackground from '/authPage.jpg';
 
 import style from './index.module.css';
 
 export default function AuthLayout({ form, title, caption }) {
+  const location = useLocation();
+
+  const isLogginPage = location.pathname === '/login';
+  const paddingTop = isLogginPage ? '15%' : '10%';
+
   return (
     <div className={style.container}>
-      <InfoBlock caption={caption} title={title} />
 
-      <div className={style.form}>
-        {form}
+      <div style={{ paddingTop }} className={style.wrapper}>
+        <InfoBlock caption={caption} title={title} />
+
+        <div className={style.form}>
+          {form}
+        </div>
       </div>
+
+      <div className={style.image}>
+        <Image imageUrl={authBackground} />
+      </div>
+
     </div>
   );
 }
