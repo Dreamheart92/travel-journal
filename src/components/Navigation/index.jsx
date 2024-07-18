412cb7542f2d597cc087338de72437a1013589b4
Toni Rachev <tonirachev92@gmail.com>
2024-07-18 17:41:52 +0300

    added logo and navigation menu

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,9 +1,12 @@
 import NavigationLayout from '../../layouts/NavigationLayout';
+import Logo from './Logo';
+import NavigationMenu from './NavigationMenu';
 
 export default function Navigation() {
   return (
     <NavigationLayout>
-
+      <Logo />
+      <NavigationMenu />
     </NavigationLayout>
   );
 }
