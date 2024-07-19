169d647baebc91e0038d8f79d37d0b9fe9ea102b
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 14:51:14 +0300

    fetching + passing destinations

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,22 @@
+import useFetch from '../../hooks/useFetch';
+import journalService from '../../services/journalService';
+
 import Container from '../../components/Container';
 import Hero from './Hero';
+import Destinations from './Destinations';
+import Loading from '../../components/Loading';
 
 export default function Home() {
+  const { data: destinations, isLoading } = useFetch(journalService.getDestinations);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Container>
       <Hero />
+      <Destinations destinations={destinations.data} />
     </Container>
   );
 }
