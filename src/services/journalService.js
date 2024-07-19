fc91390c8d34f6db3d5e2bec5dcd566b4b9c5f1a
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 11:47:58 +0300

    added getDestinations service + exporting services as default

--- src/services/journalService.js
+++ src/services/journalService.js
@@ -1,4 +1,10 @@
 import sendHttpRequest from './sendHttpRequest';
 import API from '../constants/api';
 
-export const getJournals = () => sendHttpRequest(API.JOURNAL.JOURNAL);
+const getJournals = () => sendHttpRequest(API.JOURNAL.JOURNAL);
+const getDestinations = () => sendHttpRequest(API.JOURNAL.DESTINATIONS);
+
+export default {
+  getJournals,
+  getDestinations,
+};
