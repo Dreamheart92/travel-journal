f4bf859f8cabbd4b92951ad82709093471140d51
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 19:02:11 +0300

    fetching + rendering latest journals

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,11 +5,22 @@ import Container from '../../components/Container';
 import Hero from '../../components/Hero';
 import Destinations from '../../components/Destinations';
 import Loading from '../../components/Loading';
+import LatestJournals from '../../components/LatestJournals';
 
 export default function Home() {
-  const { data: destinations, isLoading } = useFetch(journalService.getDestinations);
+  const {
+    data: destinations,
+    isLoading: isDestinationsLoading,
+    error: destinationsError
+  } = useFetch(journalService.getDestinations);
 
-  if (isLoading) {
+  const {
+    data: journalsData,
+    isLoading: isJournalsLoading,
+    error: journalsError
+  } = useFetch(journalService.getJournals);
+
+  if (isDestinationsLoading || isJournalsLoading) {
     return <Loading />;
   }
 
@@ -17,6 +28,7 @@ export default function Home() {
     <Container>
       <Hero />
       <Destinations destinations={destinations.data} />
+      <LatestJournals journals={journalsData.data.journals} />
     </Container>
   );
 }
