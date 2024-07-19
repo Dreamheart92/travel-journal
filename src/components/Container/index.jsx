fbd543147f361840cf11f5d821be4643f0d98452
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 14:50:08 +0300

    added heading prop + conditional rendering

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,9 +2,13 @@ import PropTypes from 'prop-types';
 
 import style from './index.module.css';
 
-export default function Container({ children }) {
+export default function Container({ children, heading }) {
   return (
     <div className={style.container}>
+
+      {heading
+        && <h1 className={style.heading}>{heading}</h1>}
+
       {children}
     </div>
   );
@@ -12,4 +16,5 @@ export default function Container({ children }) {
 
 Container.propTypes = {
   children: PropTypes.node.isRequired,
+  heading: PropTypes.string,
 };
