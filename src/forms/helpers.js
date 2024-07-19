32aa22f71702a3e27aeef3527ca54d53fa3f0d27
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 11:23:56 +0300

    Delete helpers.js

--- src/forms/helpers.js
+++
@@ -1,9 +0,0 @@
-const constructLoginData = (loginData) => {
-  const data = {
-    email: loginData.email.value,
-    password: loginData.password.value,
-  };
-  return data;
-};
-
-export default constructLoginData;
