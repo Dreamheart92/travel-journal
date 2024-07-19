99c65c6b097e8fbe7bda53056b59980acce3774d
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 14:50:23 +0300

    created destination card

---
+++ src/components/DestinationCard/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Image from '../Image';
+import InfloBlock from '../InfoBlock';
+
+import { PATHS } from '../../constants/paths';
+
+import style from './index.module.css';
+import destinationPropTypes from '../../propTypes/destinationPropTypes';
+
+export default function DestinationCard({ destination }) {
+  const [isHover, setIsHover] = useState(false);
+
+  const cardImageClass = `${style['card-image']} ${isHover ? style.hover : ''}`;
+  const infoClass = `${style.info} ${isHover ? style.active : ''}`;
+
+  return (
+    <Link to={PATHS.CATALOGUE}>
+      <div
+        className={style['destination-card']}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+
+        <div className={cardImageClass}>
+          <Image imageUrl={destination.imageUrl} />
+        </div>
+
+        <div className={infoClass}>
+          <InfloBlock
+            title={destination.name}
+            caption={destination.description}
+          />
+        </div>
+
+        <div className={style.count}>
+          <p>
+            {destination.count}
+            {destination.count !== 1 ? 'journals' : 'journal'}
+          </p>
+        </div>
+
+      </div>
+    </Link>
+  );
+}
+
+DestinationCard.propTypes = destinationPropTypes;
