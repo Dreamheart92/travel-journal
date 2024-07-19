aeb19df1b88336d3e3090b33a3cd1765b20618fd
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 20:18:07 +0300

    added prop types

--- src/components/Navigation/NavigationMenu/index.jsx
+++ src/components/Navigation/NavigationMenu/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import NavigationLink from '../NavigationLink';
 import { PATHS } from '../../../constants/paths';
@@ -13,9 +14,13 @@ export default function NavigationMenu({ onLogout }) {
     <div>
       <ul className={style.navigation}>
         <NavigationLink to={PATHS.HOME}>Home</NavigationLink>
-        <NavigationLink to={PATHS.CATALOGUE}>Journals</NavigationLink>
+        <NavigationLink to={`${PATHS.CATALOGUE}`}>Journals</NavigationLink>
         {user ? <UserNavigation onLogout={onLogout} /> : <GuestNavigation />}
       </ul>
     </div>
   );
 }
+
+NavigationMenu.propTypes = {
+  onLogout: PropTypes.func.isRequired,
+};
