c4fe047e13c83b3cbd96236a4004d22d07107295
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 15:28:26 +0300

    renamed to prop types

--- src/propTypes/commentPropTypes.js
+++ src/propTypes/commentPropTypes.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 
-const commentPropType = {
+const commentPropTypes = {
   _id: PropTypes.string,
   author: PropTypes.string,
   comment: PropTypes.string,
   likes: PropTypes.arrayOf(PropTypes.string),
   dislikes: PropTypes.arrayOf(PropTypes.string),
   totalLikes: PropTypes.number,
-  createdAt: PropTypes.string
+  createdAt: PropTypes.string,
 };
 
-export default commentPropType;
+export default commentPropTypes;
