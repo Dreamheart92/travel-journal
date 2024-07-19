edfe1f016cc37b46dfe3328b223c3fc3dd70ec1d
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 11:24:56 +0300

    adding style depending on location

--- src/layouts/NavigationLayout/index.jsx
+++ src/layouts/NavigationLayout/index.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 
 import style from './index.module.css';
 
 export default function NavigationLayout({ children }) {
+  const location = useLocation();
+
+  const isOnHomePage = location.pathname === '/';
+  const headerClass = `${style.header} ${isOnHomePage ? style['home-page'] : ''}`;
+
   return (
-    <header className={style.header}>
+    <header className={headerClass}>
       {children}
     </header>
   );
