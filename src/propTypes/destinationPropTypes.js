6774e89bde9c540109059550003c4957e54ebc67
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 14:37:18 +0300

    created destination prop types

---
+++ src/propTypes/destinationPropTypes.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const destinationPropTypes = {
+  _id: PropTypes.string,
+  count: PropTypes.number,
+  description: PropTypes.string,
+  imageUrl: PropTypes.string,
+  name: PropTypes.string,
+};
+
+export default destinationPropTypes;
