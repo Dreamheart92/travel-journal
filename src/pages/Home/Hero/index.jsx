ef0ab2b57c5f03ad74f83bdfd1ee1c079f40d86e
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 11:34:16 +0300

    added banner + info block

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -1,6 +1,22 @@
 import Image from '../../../components/Image';
+import InfoBlock from '../../../components/InfoBlock';
+
 import heroBanner from '/hero.jpg';
+import style from './index.module.css';
 
 export default function Hero() {
-  return <Image imageUrl={heroBanner} />;
+  return (
+    <div className={style.hero}>
+      <div className={style.banner}>
+        <Image imageUrl={heroBanner} />
+      </div>
+
+      <div className={style.caption}>
+        <InfoBlock
+          title="Discover the World Through Stories"
+          caption="Share your adventures. Explore new destinations. Connect with fellow travelers."
+        />
+      </div>
+    </div>
+  );
 }
