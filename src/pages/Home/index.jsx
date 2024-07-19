296d34ef4d64fd34fc4a3c061f1a0607f60180a4
Toni Rachev <tonirachev92@gmail.com>
2024-07-19 11:34:34 +0300

    wrapped with container + hero

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,10 @@
+import Container from '../../components/Container';
 import Hero from './Hero';
 
 export default function Home() {
+  return (
+    <Container>
+      <Hero />
+    </Container>
+  );
 }
