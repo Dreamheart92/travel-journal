e19c83a646e3d0ad00ce8a722912d1a895b12cc9
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 11:21:29 +0300

    created catalogue card button

---
+++ src/components/CatalogueCard/CatalogueCardButton/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import style from './index.module.css';
+
+export default function CatalogueCardButton({ caption }) {
+  return (
+    <button
+      className={style.button}
+      type="button"
+    >
+      <span className={style.visible}>
+        {caption}
+      </span>
+      <FontAwesomeIcon className={style['arrow-icon']} icon={faArrowRight} />
+      <span className={style.hidden}>{caption}</span>
+    </button>
+  );
+}
+
+CatalogueCardButton.propTypes = {
+  caption: PropTypes.string.isRequired,
+};
