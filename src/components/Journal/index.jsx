f0ff47498f9c75dadc35dc87373cd48e92d7d9d8
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 22:02:11 +0300

    created journal component

---
+++ src/components/Journal/index.jsx
@@ -0,0 +1,19 @@
+import style from './index.module.css';
+
+export default function Journal(
+  {
+    id,
+    title,
+    location,
+    author,
+    date,
+    imageUrl,
+    description,
+  },
+) {
+  return (
+    <div className={style.container}>
+
+    </div>
+  );
+}
