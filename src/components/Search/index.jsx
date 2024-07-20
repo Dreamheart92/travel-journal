4abdb02ba60e60f9a9c6830d7c15ac29df3bab40
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 21:19:10 +0300

    added props + debouncing func

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import SidebarSection from '../Sidebar/SidebarSection';
 import style from './index.module.css';
 
-export default function Search() {
-  const [search, setSearch] = useState('');
+export default function Search(
+  {
+    onQuery,
+    urlSearch,
+    isSearching,
+    onSearching,
+  },
+) {
+  const [search, setSearch] = useState(urlSearch);
+
+  const debounceQueryTimeout = useRef(null);
 
   const handleChange = (event) => {
+    if (!isSearching) {
+      onSearching(true);
+    }
+
     setSearch(event.target.value);
   };
+
+  useEffect(() => {
+    if (isSearching) {
+      debounceQueryTimeout.current = setTimeout(() => {
+        onQuery('search', search);
+      }, 500);
+    }
+
+    return () => {
+      if (isSearching && debounceQueryTimeout.current) {
+        clearTimeout(debounceQueryTimeout.current);
+      }
+    };
+  }, [search]);
 
   return (
     <SidebarSection
@@ -30,3 +58,10 @@ export default function Search() {
     </SidebarSection>
   );
 }
+
+Search.propTypes = {
+  onQuery: PropTypes.func,
+  urlSearch: PropTypes.string,
+  isSearching: PropTypes.bool,
+  onSearching: PropTypes.func,
+};
