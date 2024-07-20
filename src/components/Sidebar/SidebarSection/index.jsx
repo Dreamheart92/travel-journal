0e0f462711efeb6010a2eeca497e8491a67586aa
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 11:43:35 +0300

    added div classname

--- src/components/Sidebar/SidebarSection/index.jsx
+++ src/components/Sidebar/SidebarSection/index.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 
+import style from './index.module.css';
+
 export default function SidebarSection({ heading, children }) {
   return (
-    <div>
+    <div className={style['sidebar-section']}>
       <h3>{heading}</h3>
       {children}
     </div>
