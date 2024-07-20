20c01e54459212b41716b31d6c9c94176cec9e6a
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 17:30:50 +0300

    refactor to use sendHttpRequest for API calls

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,29 +1,49 @@
 import { useEffect, useState } from 'react';
+import sendHttpRequest from '../services/sendHttpRequest';
 
-const useFetch = (fetchFunction) => {
+const useFetch = (fetchCallback) => {
   const [data, setData] = useState();
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const resetLoadingAndSuccessState = () => {
+    setIsLoading(true);
+    setIsSuccess(false);
+  };
 
   useEffect(() => {
+    const controller = new AbortController();
+    resetLoadingAndSuccessState();
+
     const fetchData = async () => {
       try {
-        const fetchedData = await fetchFunction();
+        const fetchedData = await sendHttpRequest(
+          {
+            ...fetchCallback(),
+            signal: controller.signal,
+          },
+        );
         setData(fetchedData);
+        setIsSuccess(true);
       } catch (fetchError) {
         setError(fetchError);
       } finally {
         setIsLoading(false);
       }
     };
-
     fetchData();
-  }, []);
+
+    return () => {
+      controller.abort();
+    };
+  }, [fetchCallback]);
 
   return {
     data,
     error,
     isLoading,
+    isSuccess,
   };
 };
 
