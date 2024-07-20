8aef712d481477a229f9bc7be244d6affc8b49e9
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 21:18:44 +0300

    removed container

--- src/components/JournalsList/index.jsx
+++ src/components/JournalsList/index.jsx
@@ -1,19 +1,18 @@
 import PropTypes from 'prop-types';
 
-import Container from '../Container';
 import CatalogueCard from '../CatalogueCard';
 import journalPropTypes from '../../propTypes/journalPropTypes';
 
 export default function JournalsList({ journals }) {
   return (
-    <Container>
+    <>
       {journals.map((journal) => (
         <CatalogueCard
           key={journal._id}
           journal={journal}
         />
       ))}
-    </Container>
+    </>
   );
 }
 
