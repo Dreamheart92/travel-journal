f0ccef51277d68b5ccabcdbd08607616386244b0
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 22:02:57 +0300

    fetching and rendering journal

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,45 @@
-import style from './index.module.css';
+import { useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import journalServiceSettings from '../../services/journalServiceSettings';
+import Loading from '../../components/Loading';
+import DefaultLayout from '../../layouts/DefaultLayout';
+import Journal from '../../components/Journal';
 
 export default function Details() {
+  const { journalId } = useParams();
 
-};
+  const {
+    data: journalData,
+    isSuccess,
+    error,
+  } = useFetch(useCallback(() => journalServiceSettings.getJournalByIdSettings(journalId), []));
+
+  if (!isSuccess) {
+    return <Loading />;
+  }
+
+  const {
+    _id,
+    title,
+    location,
+    author,
+    date,
+    imageUrl,
+    description,
+  } = journalData.data;
+
+  return (
+    <DefaultLayout>
+      <Journal
+        id={_id}
+        title={title}
+        location={location}
+        author={author}
+        date={date}
+        imageUrl={imageUrl}
+        description={description}
+      />
+    </DefaultLayout>
+  );
+}
