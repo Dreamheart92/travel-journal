2a83c02e5ff5e25c5d95e5b92ee75e9f5ee328ab
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 22:10:11 +0300

    added styling and date formatting

--- src/components/Journal/JournalHeader/index.jsx
+++ src/components/Journal/JournalHeader/index.jsx
@@ -1,5 +1,29 @@
-export default function JournalHeader() {
-  return (
+import { formatDate } from '../../../utility/utility';
+import style from './index.module.css';
 
-  )
-};
+export default function JournalHeader(
+  {
+    title,
+    location,
+    author,
+    date,
+    id,
+  },
+) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <h4>{location}</h4>
+      <p>
+        By:
+        {author}
+      </p>
+      <div className={style.wrapper}>
+        <p>
+          Date:
+          {formatDate(date)}
+        </p>
+      </div>
+    </header>
+  );
+}
