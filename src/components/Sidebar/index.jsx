ae501b703aa0ef6f4011755b2f08207e12886d4b
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 11:36:05 +0300

    added proptypes + width prop with default value

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,19 @@
-export default function Sidebar() {
-  return (
-    <div>
+import PropTypes from 'prop-types';
 
+import style from './index.module.css';
+
+export default function Sidebar({ width = '100%', children }) {
+  return (
+    <div
+      style={{ width }}
+      className={style.sidebar}
+    >
+      {children}
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  width: PropTypes.string,
+  children: PropTypes.node,
+};
