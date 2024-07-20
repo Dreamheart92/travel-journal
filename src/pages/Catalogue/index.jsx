c4dc4ab4426a23ccd3e99cc8787d13c8400b3f5f
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 11:23:46 +0300

    wrapped journals list and sidebar

--- src/pages/Catalogue/index.jsx
+++ src/pages/Catalogue/index.jsx
@@ -4,8 +4,9 @@ import journalService from '../../services/journalService';
 import Loading from '../../components/Loading';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import DestinationHeader from '../../components/DestinationHeader';
-import Container from '../../components/Container';
 import JournalsList from '../../components/JournalsList';
+import Sidebar from '../../components/Sidebar';
+import style from './index.module.css';
 
 export default function Catalogue() {
   const { destination } = useParams();
@@ -33,7 +34,10 @@ export default function Catalogue() {
   return (
     <DefaultLayout>
       <DestinationHeader destination={currentDestination} />
-      <JournalsList journals={journals.data.journals} />
+      <div className={style.wrapper}>
+        <JournalsList journals={journals.data.journals} />
+        <Sidebar width="20%" />
+      </div>
     </DefaultLayout>
   );
 }
