4dff3f5895b941b793fef60dcc634b3467c47276
Toni Rachev <tonirachev92@gmail.com>
2024-07-20 11:24:03 +0300

    created format date helper

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -1,1 +1,5 @@
+import moment from 'moment';
+
 export const normalizeName = (value) => value[0].toUpperCase() + value.slice(1);
+
+export const formatDate = (date) => moment(new Date(date)).format('MMMM Do YYYY');
