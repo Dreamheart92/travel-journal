ddf0ca586c1559586ee50a30231ffe82a6427bc6
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 11:10:36 +0300

    added split by new line func

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -5,3 +5,5 @@ export const normalizeName = (value) => value[0].toUpperCase() + value.slice(1);
 export const formatDate = (date) => moment(new Date(date)).format('MMMM Do YYYY');
 
 export const convertToRelativeTime = (date) => moment(new Date(date)).fromNow();
+
+export const splitByNewLine = (content) => content.split('\n');
