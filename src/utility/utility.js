5034813d640ccaadec6fc31dd9669cf1fb2471bd
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 11:02:25 +0300

    added convert to relative time func

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -3,3 +3,5 @@ import moment from 'moment';
 export const normalizeName = (value) => value[0].toUpperCase() + value.slice(1);
 
 export const formatDate = (date) => moment(new Date(date)).format('MMMM Do YYYY');
+
+export const convertToRelativeTime = (date) => moment(new Date(date)).fromNow();
