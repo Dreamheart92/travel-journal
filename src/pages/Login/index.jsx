fbc68ede3c8e0471bdcc49f9d41b7afc4d8ddf97
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 17:08:40 +0300

    consuming useOnFetch, created handleLoginSubmit and useEffect for success login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import LoginForm from '../../forms/LoginForm';
-
 import AuthLayout from '../../layouts/AuthLayout';
+import useOnFetch from '../../hooks/useOnFetch';
+import authService from '../../services/authService';
+import { constructLoginData } from '../../forms/helpers/loginForm';
+import { storeUserData } from '../../helpers/storage';
+import { PATHS } from '../../constants/paths';
 
 export default function Login() {
+  const navigate = useNavigate();
+
+  const {
+    data: userData,
+    isLoading,
+    error,
+    fetch: submitLogin,
+  } = useOnFetch();
+
+  const handleLoginSubmit = (formData) => {
+    submitLogin(authService.login(constructLoginData(formData)));
+  };
+
+  useEffect(() => {
+    if (userData) {
+      storeUserData(userData.data);
+      navigate(PATHS.HOME);
+    }
+  }, [userData]);
+
   return (
     <AuthLayout
       title="Welcome Back, Explorer!"
       caption="Log in to your Travel Journal and continue your journey of sharing adventures and discovering new destinations."
-      form={<LoginForm />}
+      form={(
+        <LoginForm
+          onLoginSubmit={handleLoginSubmit}
+          isSubmitting={isLoading}
+          error={error}
+        />
+      )}
     />
   );
 }
