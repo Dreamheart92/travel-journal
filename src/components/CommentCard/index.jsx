1e36177d5add2ca1d8db280f1ae1fb63c975e19b
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 11:14:45 +0300

    rendering comment card content

--- src/components/CommentCard/index.jsx
+++ src/components/CommentCard/index.jsx
@@ -1,15 +1,25 @@
 import UserProfileImage from '../UserProfileImage';
 
 import style from './index.module.css';
+import CommentCardContent from './CommentCardContent';
 
 export default function CommentCard(
   {
     comment,
   },
 ) {
+  const { author, createdAt, comment: content, _id: id } = comment;
+
   return (
     <div className={style['comment-card']}>
-      <UserProfileImage imageUrl={comment.author.imageUrl} />
+      <UserProfileImage imageUrl={author.imageUrl} />
+
+      <CommentCardContent
+        author={author.username}
+        content={content}
+        createdAt={createdAt}
+        id={id}
+      />
     </div>
   );
 }
