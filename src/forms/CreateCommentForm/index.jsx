e5f1a028876f010b846bbd1ba1a353623939076e
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 19:52:20 +0300

    moved useOnFetch and useEffect for handling the comment request in the parent

--- src/forms/CreateCommentForm/index.jsx
+++ src/forms/CreateCommentForm/index.jsx
@@ -27,46 +27,15 @@ export default function CreateCommentForm({ user, journalId }) {
 
   const {
     register,
-    formData: commentData,
     handleSubmit,
     clearFieldValue,
   } = useForm();
-
-  const {
-    data: submittedCommentData,
-    error,
-    fetch: submitNewComment,
-  } = useOnFetch();
 
   const [isCommentEmpty, setIsCommentEmpty] = useState(false);
   const [isTyping, setIsTyping] = useState(false);
 
   const { handlers, state } = register('comment', '');
   const temporaryLocalCommentId = useRef(null);
-
-  useEffect(() => {
-    if (commentData) {
-      if (commentData.comment.value.trim() === '') {
-        handleEmptyComment(setIsCommentEmpty, setIsTyping);
-      } else {
-        clearFieldValue('comment');
-        temporaryLocalCommentId.current = buildTemporaryCommentId();
-
-        const localComment = constructLocalComment(
-          user,
-          commentData.comment.value,
-          temporaryLocalCommentId.current,
-        );
-
-        onAddLocalComment(localComment);
-
-        submitNewComment(commentService.createComment({
-          comment: commentData.comment.value,
-          createdAt: localComment.createdAt,
-        }, journalId));
-      }
-    }
-  }, [commentData]);
 
   useEffect(() => {
     if (submittedCommentData) {
