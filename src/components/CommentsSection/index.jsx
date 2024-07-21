c9f68709edc9ad9fb180d70d30072dc4d0800015
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 10:39:29 +0300

    conditional rendering header caption

--- src/components/CommentsSection/index.jsx
+++ src/components/CommentsSection/index.jsx
@@ -1,5 +1,20 @@
-import style from './index.module.css';
+import CommentsSectionHeader from './CommentsSectionHeader';
+import CreateCommentForm from '../../forms/CreateCommentForm';
 
-export default function CommentsSection() {
+export default function CommentsSection(
+  {
+    user,
+    comments,
+  },
+) {
+  const caption = user ? 'Leave a comment' : 'Login to leave a comment';
 
+  return (
+    <>
+      <CommentsSectionHeader caption={caption} />
+
+      {user
+        && <CreateCommentForm />}
+    </>
+  );
 }
