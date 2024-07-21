ffd26dba7932ab85d491fb104d2ac56d416688a1
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 19:44:48 +0300

    created handleSignupSubmit, added passwordsNotMatching state and using useEffect to handle success signup

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useOnFetch from '../../hooks/useOnFetch';
 import AuthLayout from '../../layouts/AuthLayout';
-
 import SignupForm from '../../forms/SignupForm';
+import { constructSignupData } from '../../forms/helpers/signupForm';
+import { storeUserData } from '../../helpers/storage';
+import { PATHS } from '../../constants/paths';
+import authService from '../../services/authService';
 
 export default function Signup() {
+  const navigate = useNavigate();
+
+  const {
+    data: success,
+    isLoading,
+    error,
+    fetch: sendSignupRequest,
+  } = useOnFetch();
+
+  const [passwordsNotMatching, setPasswordsNotMatching] = useState(false);
+
+  const handleSignupSubmit = (signupData) => {
+    setPasswordsNotMatching(false);
+
+    if (signupData.password !== signupData.confirmPassword) {
+      setPasswordsNotMatching(true);
+      return;
+    }
+
+    sendSignupRequest(authService.signup(constructSignupData(signupData)));
+  };
+
+  useEffect(() => {
+    if (success) {
+      storeUserData(success.data);
+      navigate(PATHS.HOME);
+    }
+  }, [success]);
+
   return (
     <AuthLayout
       title="Join the Adventure!"
       caption="Sign up to create your Travel Journal, share your stories, and explore a world of new destinations with fellow travelers."
-      form={<SignupForm />}
+      form={(
+        <SignupForm
+          onSignupSubmit={handleSignupSubmit}
+          isSubmitting={isLoading}
+          error={passwordsNotMatching ? null : error}
+          passwordsNotMatching={passwordsNotMatching}
+        />
+      )}
     />
   );
 }
