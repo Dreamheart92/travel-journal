d702d0a1c23235a5d395ec70242b60f323f8b110
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 13:06:08 +0300

    renamed to commentService

---
+++ src/services/commentService.js
@@ -0,0 +1,22 @@
+import API from '../constants/api';
+import { getAccessTokenAndId } from '../utility/storage';
+import sendHttpRequest from './sendHttpRequest';
+
+const createComment = (commentData, journalId) => {
+  const { accessToken } = getAccessTokenAndId();
+
+  const settings = {
+    method: 'Post',
+    headers: {
+      Authorization: accessToken,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(commentData),
+  };
+
+  return sendHttpRequest(`${API.COMMENTS.COMMENTS}/${journalId}`, settings);
+};
+
+export default {
+  createComment,
+};
