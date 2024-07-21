61c88c40ca4b25ffe926dd35d7d20f67477389f7
Toni Rachev <tonirachev92@gmail.com>
2024-07-21 23:46:24 +0300

    removed caption and passing user bool to header

--- src/components/CommentsSection/index.jsx
+++ src/components/CommentsSection/index.jsx
@@ -26,14 +26,12 @@ export default function CommentsSection(
 
   
 
-  const caption = user ? 'Leave a comment' : 'Login to leave a comment';
 
   const totalComments = () => `${comments.length} comment${comments.length > 1 ? 's' : ''}`;
 
   return (
     <>
-      <CommentsSectionHeader caption={caption} />
-
+      <CommentsSectionHeader user={!!user} />
       {user
         && <CreateCommentForm user={user} journalId={journalId} />}
 
