522b00fd737c99d845903810d4af62475eb71a86
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:59:21 +0300

    added post comment thunk

--- src/store/details/thunks.js
+++ src/store/details/thunks.js
@@ -1,10 +1,31 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import sendHttpRequest from '../../services/sendHttpRequest';
 import API from '../../constants/api';
+import { getAccessTokenAndId } from '../../helpers/storage';
 
 export const fetchJournalById = createAsyncThunk(
   'details/fetchJournalById',
   async (journalId, { signal }) => {
     return sendHttpRequest(`${API.JOURNAL.JOURNAL}/${journalId}`);
+  },
+);
+
+export const postComment = createAsyncThunk(
+  'details/postComment',
+  async (commentMetaData, { signal }) => {
+    const { commentData, journalId } = commentMetaData;
+
+    const { accessToken } = getAccessTokenAndId();
+
+    const settings = {
+      method: 'Post',
+      headers: {
+        Authorization: accessToken,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentData),
+    };
+
+    return sendHttpRequest(`${API.COMMENTS.COMMENTS}/${journalId}`, settings);
   },
 );
