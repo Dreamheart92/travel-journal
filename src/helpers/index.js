bdb0d5773ac479b8f9d7fe48cd0c060dafa34905
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:52:56 +0300

    added build journal url request refactor later when add sort

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import API from '../constants/api';
 
 export const normalizeName = (value) => value[0].toUpperCase() + value.slice(1);
 
@@ -9,3 +10,19 @@ export const convertToRelativeTime = (date) => moment(new Date(date)).fromNow();
 export const splitByNewLine = (content) => content.split('\n');
 
 export const formatCommentsCount = (comments) => `${comments.length} comment${comments.length > 1 ? 's' : ''}`;
+
+export const buildJournalUrlRequest = (queryParams) => {
+  if (queryParams) {
+    const { search, destination } = queryParams;
+
+    let query = '?';
+
+    if (search) {
+      query += `search=${search}`;
+    }
+
+    return `${API.JOURNAL.JOURNALS}/${destination || ''}${query}`;
+  }
+
+  return API.JOURNAL.JOURNALS;
+};
