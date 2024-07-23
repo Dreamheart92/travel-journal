236245c28c0c820e1e3e2a4f8c830d4b3ce0cabe
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:57:37 +0300

    added fetch destinations states

--- src/store/destinations/index.js
+++ src/store/destinations/index.js
@@ -7,9 +7,26 @@ const initialState = {
   error: null,
   success: false,
 };
+
 const destinationsSlice = createSlice({
   name: 'destinations',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchDestinations.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchDestinations.fulfilled, (state, action) => {
+      state.destinations = action.payload.data;
+      state.loading = false;
+      state.success = true;
+    });
+    builder.addCase(fetchDestinations.rejected, (state, action) => {
+      state.loading = false;
+      state.error = true;
+    });
+  },
 });
+
 export default destinationsSlice.reducer;
