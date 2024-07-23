feb212b03cc71e2bb9bc94bf5b083d4a781a3cf1
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:25:31 +0300

    moved handleUserStorageChange outside the useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import Navigation from './components/Navigation';
 import { userActions } from './store/userSlice';
 import { getUserDataFromStorage } from './helpers/storage';
 import Loading from './components/Loading';
+import { selectDestinations } from './store/destinations/selectors';
+import { fetchDestinations } from './store/destinations/thunks';
 
 export default function App() {
   const dispatch = useDispatch();
   const [initApp, setInitApp] = useState(false);
 
-  useEffect(() => {
-    const handleUserStorageChange = (event) => {
-      if (event.detail.type === 'update') {
-        dispatch(userActions.storeUser({ userData: event.detail.data }));
-      } else if (event.detail.type === 'remove') {
-        dispatch(userActions.deleteUser());
-      }
-    };
 
+  const handleUserStorageChange = useCallback((event) => {
+    if (event.detail.type === 'update') {
+      dispatch(userActions.storeUser({ userData: event.detail.data }));
+    } else if (event.detail.type === 'remove') {
+      dispatch(userActions.deleteUser());
+    }
+  }, []);
+
+  useEffect(() => {
     window.addEventListener('userStorageChange', handleUserStorageChange);
 
     return () => {
