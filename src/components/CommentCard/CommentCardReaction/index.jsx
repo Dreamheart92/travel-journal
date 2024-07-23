6162b0ef37ce4604694080775be5e2f90c7a7ae9
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:33:09 +0300

    accepting props

--- src/components/CommentCard/CommentCardReaction/index.jsx
+++ src/components/CommentCard/CommentCardReaction/index.jsx
@@ -1,4 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp as faSolidThumbsUp } from '@fortawesome/free-solid-svg-icons/faThumbsUp';
+import { faThumbsUp as faRegularThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faThumbsDown as faSolidThumbsDown } from '@fortawesome/free-solid-svg-icons/faThumbsDown';
+import { faThumbsDown as faRegularThumbsDown } from '@fortawesome/free-regular-svg-icons/faThumbsDown';
 import style from './index.module.css';
 
-export default function CommentCardReaction() {
+export default function CommentCardReaction(
+  {
+    isLikedComment,
+    isDislikedComment,
+    likes,
+    dislikes,
+    onCommentReaction,
+  },
+) {
 }
