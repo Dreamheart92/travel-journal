f98fc5a556ce84e13d5612ce9bb058dcff5cbe97
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:55:28 +0300

    refactored journal fetch and changed loading conditional

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,56 +1,40 @@
-import { useSelector } from 'react-redux';
-import { useCallback, useContext, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
-import journalServiceSettings from '../../services/journalServiceSettings';
 import Loading from '../../components/Loading';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import Journal from '../../components/Journal';
 import CommentsSection from '../../components/CommentsSection';
 import Container from '../../components/Container';
-import { CommentsContext } from '../../context/CommentsContext';
+import { fetchJournalById } from '../../store/details/thunks';
+import { selectDetailsState } from '../../store/details/selectors';
 
 export default function Details() {
   const { journalId } = useParams();
   const { user } = useSelector((state) => state.user);
-  const { comments, onInitComments } = useContext(CommentsContext);
 
-  const {
-    data,
-    isSuccess,
-    error,
-  } = useFetch(useCallback(() => journalServiceSettings.getJournalByIdSettings(journalId), []));
+  const dispatch = useDispatch();
+  const { journal, comments, loading } = useSelector(selectDetailsState);
 
   useEffect(() => {
-    if (data) {
-      onInitComments(data.data.comments);
-    }
-  }, [data]);
+    dispatch(fetchJournalById(journalId));
+  }, [journalId]);
 
-  if (!isSuccess) {
+  if (loading || !journal) {
     return <Loading />;
   }
-
-  const {
-    title,
-    location,
-    author,
-    date,
-    imageUrl,
-    description,
-  } = data.data;
 
   return (
     <DefaultLayout>
       <Container width="80em">
         <Journal
           id={journalId}
-          title={title}
-          location={location}
-          author={author.username}
-          date={date}
-          imageUrl={imageUrl}
-          description={description}
+          title={journal.title}
+          location={journal.location}
+          author={journal.author?.username}
+          date={journal.date}
+          imageUrl={journal.imageUrl}
+          description={journal.description}
         />
 
         <CommentsSection
