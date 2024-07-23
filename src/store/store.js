2de60c1942a3a8854e4bc6a741d5e6e769db5325
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 18:02:08 +0300

    added homeReducer, destinationsReducer, catalogueReducer and details reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
+import homeReducer from './home';
+import destinationsReducer from './destinations';
+import catalogueReducer from './catalogue';
+import detailsReducer from './details';
 
 const store = configureStore({
-  reducer: { user: userSlice },
+  reducer: {
+    user: userSlice,
+    home: homeReducer,
+    destinations: destinationsReducer,
+    catalogue: catalogueReducer,
+    details: detailsReducer,
+  },
 });
 
 export default store;
