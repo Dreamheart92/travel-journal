30048a6b68e3e1ac2ceb7abd98bda9f322bcf8d0
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 20:37:18 +0300

    added useEffect to handle modal state

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,2 +1,31 @@
-export default function Modal() {
+import { useEffect, useRef } from 'react';
+
+import style from './index.module.css';
+
+export default function Modal({ isOpen, children }) {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (dialogRef.current) {
+      if (isOpen) {
+        dialogRef.current.showModal();
+        document.body.classList.add('no-scroll');
+      } else {
+        dialogRef.current.close();
+        document.body.classList.remove('no-scroll');
+      }
+    }
+  }, [isOpen]);
+
+  return (
+    <dialog
+      ref={dialogRef}
+      className={style.dialog}
+    >
+      <div className={style.content}>
+        {isOpen
+          && children}
+      </div>
+    </dialog>
+  );
 }
