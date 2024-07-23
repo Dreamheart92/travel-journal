f7d9a89b70ee580e4cfbfb6f2291225c0a4108c2
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 00:28:01 +0300

    rendering journal form with title and caption

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,3 +1,11 @@
+import JournalForm from '../../components/JournalForm';
+
 export default function Create() {
-  
+  return (
+    <JournalForm
+      title="Share your journey!"
+      caption="Create your travel journal and capture every moment of your adventure. Share your stories,
+                photos, and experiences with the world."
+    />
+  );
 }
