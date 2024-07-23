defd77d45f2a2f87f7e8da8b6dbf9adbbf046ed0
Toni Rachev <tonirachev92@gmail.com>
2024-07-23 17:52:20 +0300

    refactored and renamed buildTemporaryCommentId and refactored createdAt

--- src/forms/helpers/createCommentForm.js
+++ src/forms/helpers/createCommentForm.js
@@ -1,11 +1,11 @@
-export const constructLocalComment = (user, comment, id) => ({
-  _id: id,
-  createdAt: new Date(),
+export const buildTemporaryCommentId = () => `${Math.random() * 1000}___temporary`;
+
+export const buildLocalComment = (user, comment) => ({
+  _id: buildTemporaryCommentId(),
+  createdAt: String(new Date()),
   author: user,
   comment,
   dislikes: [],
   likes: [],
   totalLikes: 0,
 });
-
-export const buildTemporaryCommentId = () => `${Math.random() * 1000}___temporary`;
