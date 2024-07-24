a5e496851f8bf4aaa7005cbd16e362cb914e1872
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 17:03:15 +0300

    added selectDetailsState

--- src/store/details/selectors.js
+++ src/store/details/selectors.js
@@ -1,3 +1,16 @@
-export const selectDetailsState = (state) => state.details;
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectDetails = (state) => state.details;
+
+export const selectDetailsState = createSelector(
+  [selectDetails],
+  (details) => ({
+    journal: details.journal,
+    comments: details.comments,
+    loading: details.loading,
+    error: details.error,
+    isJournalOwner: details.isJournalOwner,
+  }),
+);
 
 export const selectCommentDeleteState = (state) => state.details.commentCrud.delete;
