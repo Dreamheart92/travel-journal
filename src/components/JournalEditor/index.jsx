ddaf107f8804254c93d8ccdced6428f2301cb7b5
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 16:59:14 +0300

    receiving form props

--- src/components/JournalEditor/index.jsx
+++ src/components/JournalEditor/index.jsx
@@ -1,8 +1,18 @@
 import Container from '../Container';
 import InfoBlock from '../InfoBlock';
 import DefaultLayout from '../../layouts/DefaultLayout';
+import JournalForm from '../../forms/JournalForm';
 
-export default function JournalEditor({ title, caption }) {
+export default function JournalEditor(
+  {
+    title,
+    caption,
+    type,
+    submitCallback,
+    error,
+    isSubmitting,
+  },
+) {
   return (
     <DefaultLayout>
       <Container>
