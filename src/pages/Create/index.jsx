ca29613acbf53a1913a93beff82fd1e9ed0fa56d
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 17:01:06 +0300

    consuming create state and added handleCreateSubmit function

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,11 +1,34 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { postJournal } from '../../store/journalEditor/thunks';
+import { buildJournalFormData } from '../../helpers';
+
 import JournalEditor from '../../components/JournalEditor';
+import { selectCreateState } from '../../store/journalEditor/selectors';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '../../constants/paths';
 
 export default function Create() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { loading } = useSelector(selectCreateState);
+
+  const handleCreateSubmit = async (journalData) => {
+    const requestResponse = await dispatch(postJournal(buildJournalFormData(journalData)));
+    const fetchedJournal = requestResponse.payload?.data;
+
+    if (fetchedJournal) {
+      navigate(`${PATHS.DETAILS}/${fetchedJournal._id}`);
+    }
+  };
+
   return (
     <JournalEditor
       title="Share your journey!"
       caption="Create your travel journal and capture every moment of your adventure. Share your stories,
                 photos, and experiences with the world."
+      type="journal"
+      submitCallback={handleCreateSubmit}
+      isSubmitting={loading}
     />
   );
 }
