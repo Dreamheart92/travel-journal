f2e12591316bd3aa4826d4e547d917b27c3dada2
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 18:47:48 +0300

    added update state selector

--- src/store/journalEditor/selectors.js
+++ src/store/journalEditor/selectors.js
@@ -1,3 +1,5 @@
 export const selectCreateState = (state) => state.journalEditor.create;
 
 export const selectDeleteState = (state) => state.journalEditor.delete;
+
+export const selectUpdateState = (state) => state.journalEditor.update;
