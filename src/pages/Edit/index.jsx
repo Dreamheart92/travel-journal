c4b1a9cb6fb8014041039fe25f217e12ee062bde
Toni Rachev <tonirachev92@gmail.com>
2024-07-24 18:46:19 +0300

    added use effect to fetch the journal data

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,3 +1,36 @@
-export default function Edit() {
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import JournalEditor from '../../components/JournalEditor';
+import { selectDetailsState } from '../../store/details/selectors';
+import { fetchJournalById } from '../../store/details/thunks';
+import Loading from '../../components/Loading';
+import { updateJournal } from '../../store/journalEditor/thunks';
+import { selectUpdateState } from '../../store/journalEditor/selectors';
+import { PATHS } from '../../constants/paths';
+import { journalEditorActions } from '../../store/journalEditor';
+import { buildJournalFormData } from '../../helpers';
 
+export default function Edit() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { journalId } = useParams();
+
+  const { journal, loading: journalLoading } = useSelector(selectDetailsState);
+  const { loading: isUpdating, success: isJournalUpdated } = useSelector(selectUpdateState);
+  useEffect(() => {
+    dispatch(fetchJournalById(journalId));
+  }, []);
+
+  return (
+    <JournalEditor
+      title="Refine Your Story!"
+      caption="Update your travel journal to reflect your latest adventures and insights. Edit your stories, photos, and experiences to keep your journey alive and inspiring."
+      type="Edit"
+      journal={journal}
+      submitCallback={handleUpdateJournalSubmit}
+      isSubmitting={isUpdating}
+    />
+  );
 }
