6b92665f25a2195bdab246b659a586e98fcb517a
Toni Rachev <tonirachev92@gmail.com>
2024-07-26 00:58:44 +0300

    add read state selector

--- src/store/crud/selectors.js
+++ src/store/crud/selectors.js
@@ -6,6 +6,8 @@ export const selectListState = (state) => state.crud[crudConstants.LIST];
 
 export const selectCurrentState = (state) => state.crud[crudConstants.CURRENT];
 
+export const selectReadState = (state) => state.crud[crudConstants.READ];
+
 export const selectCreateState = (state) => state.crud[crudConstants.CREATE];
 
 export const selectUpdateState = (state) => state.crud[crudConstants.UPDATE];
