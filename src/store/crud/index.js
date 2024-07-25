426208a7044fdcf987e3e85e3c7c7c8d8fa582e9
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:30:24 +0300

    created crud slice

---
+++ src/store/crud/index.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import crudConstants from '../../constants/crudConstants';
+
+const INITIAL_KEY_STATE = {
+  data: null,
+  loading: false,
+  success: false,
+  error: null,
+  currentAction: null,
+};
+
+const initialState = {
+  [crudConstants.LIST]: INITIAL_KEY_STATE,
+  [crudConstants.CURRENT]: INITIAL_KEY_STATE,
+  [crudConstants.CREATE]: INITIAL_KEY_STATE,
+  [crudConstants.READ]: INITIAL_KEY_STATE,
+  [crudConstants.UPDATE]: INITIAL_KEY_STATE,
+  [crudConstants.DELETE]: INITIAL_KEY_STATE,
+};
+
+const crudSlice = createSlice({
+  name: 'crud',
+  initialState,
+  reducers: {
+  },
+});
+
+export const crudActions = crudSlice.actions;
+export default crudSlice.reducer;
