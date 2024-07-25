95033aeeb43479027b2db9d3aad14df995c056ec
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:17:23 +0300

    Update index.jsx

--- src/components/UserProfileImage/index.jsx
+++ src/components/UserProfileImage/index.jsx
@@ -2,7 +2,6 @@ import Image from '../Image';
 import style from './index.module.css';
 
 export default function UserProfileImage({ imageUrl }) {
-
   const image = imageUrl || '/userPlaceholder.png';
 
   return (
