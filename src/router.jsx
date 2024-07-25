adc8fb6abb90b2247b84d25bc3f3be3caba60b05
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:29:24 +0300

    added account paths

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { PATHS } from './constants/paths';
+import { ACCOUNT_SUB_PATHS, PATHS } from './constants/paths';
 
 import App from './App';
 import Home from './pages/Home';
@@ -9,6 +9,10 @@ import Catalogue from './pages/Catalogue';
 import Details from './pages/Details';
 import Create from './pages/Create';
 import Edit from './pages/Edit';
+import Account from './pages/Account';
+import Dashboard from './pages/Account/Dashboard';
+import EditProfile from './pages/Account/EditProfile';
+import MyJournals from './pages/Account/MyJournals';
 
 const router = createBrowserRouter([
   {
@@ -45,6 +49,24 @@ const router = createBrowserRouter([
       {
         path: `${PATHS.EDIT}/:journalId`,
         element: <Edit />,
+      },
+      {
+        path: PATHS.ACCOUNT,
+        element: <Account />,
+        children: [
+          {
+            path: '',
+            element: <Dashboard />,
+          },
+          {
+            path: 'edit-profile',
+            element: <EditProfile />,
+          },
+          {
+            path: 'my-journals',
+            element: <MyJournals />,
+          },
+        ],
       },
     ],
   },
