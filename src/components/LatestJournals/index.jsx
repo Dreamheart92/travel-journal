30003da44867fb18369f079d8f9ddd1691b5942c
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:17:00 +0300

    added useEffect to fetch entries and consuming the entries state

--- src/components/LatestJournals/index.jsx
+++ src/components/LatestJournals/index.jsx
@@ -1,19 +1,31 @@
-import PropTypes from 'prop-types';
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from '../Container';
 import Grid from '../Grid';
 import HomeCard from '../HomeCard';
+import Loading from '../Loading';
+import { fetchEntries } from '../../store/entries/thunks';
+import { selectJournalsEntries } from '../../store/entries/selectors';
 
-import journalPropTypes from '../../propTypes/journalPropTypes';
+export default function LatestJournals() {
+  const dispatch = useDispatch();
+  const { results, loading } = useSelector(selectJournalsEntries);
 
-export default function LatestJournals({ journals }) {
+  useEffect(() => {
+    dispatch(fetchEntries());
+  }, []);
+
+  if (loading || !results) {
+    return <Loading />;
+  }
+
   return (
     <Container
       width="70%"
       heading="Latest journals"
     >
       <Grid>
-        {journals.map((journal) => (
+        {results.journals.map((journal) => (
           <HomeCard
             key={journal._id}
             journal={journal}
