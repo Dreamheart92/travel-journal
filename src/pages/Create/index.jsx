942808b4f52d6cd54190c5f2970f07279ba8458c
Toni Rachev <tonirachev92@gmail.com>
2024-07-26 01:27:19 +0300

    refactor add useEffect to handle successful new journal entry

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { buildJournalFormData } from '../../helpers';
@@ -7,22 +8,33 @@ import { postJournalRequest } from '../../store/crud/thunks';
 import crudConstants from '../../constants/crudConstants';
 import crudActionsConstants from '../../constants/crudActionsConstants';
 import { selectCreateState } from '../../store/crud/selectors';
+import { crudActions } from '../../store/crud';
 
 export default function Create() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { loading } = useSelector(selectCreateState);
 
-  const handleCreateSubmit = async (journalData) => {
-    const isSuccess = await dispatch(postJournalRequest({
+  const {
+    data: newJournalEntry,
+    loading,
+    success,
+    error,
+  } = useSelector(selectCreateState);
+
+  useEffect(() => {
+    dispatch(crudActions.resetState({ key: crudConstants.CREATE }));
+
+    if (success) {
+      navigate(`${PATHS.DETAILS}/${newJournalEntry._id}`);
+    }
+  }, [success]);
+
+  const handleCreateSubmit = (journalData) => {
+    dispatch(postJournalRequest({
       key: crudConstants.CREATE,
       currentAction: crudActionsConstants.POST_JOURNAL,
       journalData: buildJournalFormData(journalData),
     }));
-
-    if (isSuccess?.payload?.success) {
-      navigate(`${PATHS.DETAILS}/${isSuccess.payload.data._id}`);
-    }
   };
 
   return (
