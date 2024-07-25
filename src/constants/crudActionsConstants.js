ef4bb78b1a89230e4025d74df79035ba98288390
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:18:08 +0300

    created crud actions constants

---
+++ src/constants/crudActionsConstants.js
@@ -0,0 +1,11 @@
+const crudActionsConstants = {
+  POST_JOURNAL: 'post_journal',
+  UPDATE_JOURNAL: 'update_journal',
+  DELETE_JOURNAL: 'delete_journal',
+  POST_COMMENT: 'post_comment',
+  REACT_JOURNAL: 'react_journal',
+  DELETE_COMMENT: 'delete_comment',
+  UPDATE_PROFILE: 'update_profile',
+};
+
+export default crudActionsConstants;
