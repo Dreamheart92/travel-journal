9d8f005363efe947d7e4d8bb58751da3603a1c0f
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:20:02 +0300

    created dashboard page

---
+++ src/pages/Account/Dashboard/index.jsx
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux';
+import UserProfileImage from '../../../components/UserProfileImage';
+import { normalizeName } from '../../../helpers';
+
+import style from './index.module.css';
+
+export default function Dashboard() {
+  const { user } = useSelector((state) => state.user);
+
+  return (
+    <div className={style['dashboard-container']}>
+      <UserProfileImage imageUrl={user.imageUrl} />
+
+      <div>
+        <div className={style.wrapper}>
+          <p>Name</p>
+          <p>
+            {normalizeName(user.firstName)}
+            &nbsp;
+            {normalizeName(user.lastName)}
+          </p>
+        </div>
+
+        <div className={style.wrapper}>
+          <p>Username</p>
+          <p>
+            {user.username}
+          </p>
+        </div>
+
+        <div className={style.wrapper}>
+          <p>Email</p>
+          <p>
+            {user.email}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
