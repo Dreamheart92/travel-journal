6bc18b0cf4867df399621bdc4309710ea483aeb4
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:10:17 +0300

    consuming crud delete state

--- src/components/Journal/index.jsx
+++ src/components/Journal/index.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
 import Image from '../Image';
 import JournalContent from '../JournalContent';
 import style from './index.module.css';
@@ -9,28 +8,22 @@ import Button from '../Button';
 import Modal from '../Modal';
 import DeleteModal from '../Modal/DeleteModal';
 import useModal from '../../hooks/useModal';
-import { deleteJournal } from '../../store/journalEditor/thunks';
-import { selectDeleteState } from '../../store/journalEditor/selectors';
 import { PATHS } from '../../constants/paths';
-import { journalEditorActions } from '../../store/journalEditor';
+import Loading from '../Loading';
+import { fetchEntry } from '../../store/entries/thunks';
+import { selectJournalEntry } from '../../store/entries/selectors';
+import { selectDeleteState } from '../../store/crud/selectors';
+import { deleteJournalRequest } from '../../store/crud/thunks';
+import crudConstants from '../../constants/crudConstants';
+import crudActionsConstants from '../../constants/crudActionsConstants';
+import { crudActions } from '../../store/crud';
 
-export default function Journal(
-  {
-    title,
-    location,
-    author,
-    date,
-    imageUrl,
-    description,
-    isJournalOwner,
-    journalId,
-  },
-) {
+export default function Journal({ journalId }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { isOpen, onOpenModal, onCloseModal } = useModal();
-  const { success, loading } = useSelector(selectDeleteState);
+  const { loading } = useSelector(selectDeleteState);
 
   const handleDeleteJournal = () => {
     dispatch(deleteJournal(journalId));
@@ -87,14 +80,3 @@ export default function Journal(
     </div>
   );
 }
-
-Journal.propTypes = {
-  title: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  isJournalOwner: PropTypes.bool,
-  journalId: PropTypes.string.isRequired,
-};
