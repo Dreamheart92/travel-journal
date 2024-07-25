7c8da12055d3a1c6890a88de09309a1ed064af54
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:19:30 +0300

    created account layout

---
+++ src/layouts/AccountLayout/index.jsx
@@ -0,0 +1,22 @@
+import Container from '../../components/Container';
+import Image from '../../components/Image';
+import style from './index.module.css';
+
+export default function AccountLayout({ children }) {
+  return (
+    <Container width="100%">
+      <div className={style['image-header']}>
+        <Image imageUrl="/dashboard.jpg" />
+
+        <div className={style['header-title']}>
+          <h1>Dashboard</h1>
+        </div>
+      </div>
+
+
+      <div className={style['content-container']}>
+        {children}
+      </div>
+    </Container>
+  );
+}
