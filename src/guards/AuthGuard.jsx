5dcc64b88a298b6d4b4e15f76169655452dd7322
Toni Rachev <tonirachev92@gmail.com>
2024-07-26 02:25:38 +0300

    created auth guard

---
+++ src/guards/AuthGuard.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsAuthenticated } from '../store/auth/selectors';
+import { PATHS } from '../constants/paths';
+
+export default function AuthGuard({ children }) {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to={PATHS.HOME} />;
+  }
+
+  return children;
+}
