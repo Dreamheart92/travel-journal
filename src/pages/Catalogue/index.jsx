a02c59694f04d2337607e0371ec7806b7788743b
Toni Rachev <tonirachev92@gmail.com>
2024-07-25 22:25:18 +0300

    refactored

--- src/pages/Catalogue/index.jsx
+++ src/pages/Catalogue/index.jsx
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Loading from '../../components/Loading';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import DestinationHeader from '../../components/DestinationHeader';
 import JournalsList from '../../components/JournalsList';
@@ -12,31 +11,13 @@ import Search from '../../components/Search';
 import FiltersSection from '../../components/Sidebar/FiltersSection';
 import useQuery from '../../hooks/useQuery';
 import { selectDestinations } from '../../store/destinations/selectors';
-import { selectCatalogue } from '../../store/catalogue/selectors';
-import { fetchCatalogue } from '../../store/catalogue/thunks';
-import { catalogueActions } from '../../store/catalogue';
 
 export default function Catalogue() {
-  const dispatch = useDispatch();
   const { searchParams, onQuery } = useQuery();
   const { destination } = useParams();
   const [isSearching, setIsSearching] = useState(false);
 
-  const {
-    catalogue,
-    loading,
-  } = useSelector(selectCatalogue);
-
   const { destinations } = useSelector(selectDestinations);
-
-  useEffect(() => {
-    const search = searchParams.get('search');
-    dispatch(fetchCatalogue({ search, destination }));
-
-    return () => {
-      dispatch(catalogueActions.resetState());
-    };
-  }, [destination, searchParams]);
 
   const handleSearching = () => {
     setIsSearching(true);
@@ -46,20 +27,13 @@ export default function Catalogue() {
     ? destinations.find((destinationFilter) => destinationFilter.name === destination)
     : null;
 
-  if (loading || !catalogue) {
-    return <Loading />;
-  }
-
   return (
     <DefaultLayout>
       <DestinationHeader destination={currentDestination} />
       <div className={style.wrapper}>
 
         <div className={style['journals-container']}>
-          {/*{isSearching && journalsLoading*/}
-          {/*  && <Loading />}*/}
-
-          <JournalsList journals={catalogue} />
+          <JournalsList destination={destination} searchParams={searchParams} />
         </div>
 
         <Sidebar width="20em">
@@ -70,7 +44,7 @@ export default function Catalogue() {
             urlSearch={searchParams.get('search')}
             onQuery={onQuery}
           />
-          <FiltersSection destinations={destinations} />
+          <FiltersSection />
         </Sidebar>
 
       </div>
