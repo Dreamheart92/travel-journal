3d6e7de1de1fd64a569bb18b38a9894b2f5648fa
Toni Rachev <tonirachev92@gmail.com>
2024-07-26 00:57:42 +0300

    add isAuthenticated selector

--- src/store/auth/selectors.js
+++ src/store/auth/selectors.js
@@ -2,3 +2,4 @@ export const selectAuth = (state) => state.auth;
 
 export const selectUser = (state) => state.auth.user;
 
+export const selectIsAuthenticated = (state) => state.auth.isAuthenticated;
