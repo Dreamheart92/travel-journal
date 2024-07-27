503ccd8a7b1be855d0de24ae84129c97b8693174
Toni Rachev <tonirachev92@gmail.com>
2024-07-27 04:38:22 +0300

    add validate field func

--- src/context/FormContext.jsx
+++ src/context/FormContext.jsx
@@ -6,6 +6,47 @@ import {
   useState,
 } from 'react';
 
+const validateField = (validations, fieldValue) => {
+  const errors = [];
+
+  validations.forEach((validationEntry) => {
+    const [ruleEntry, messageEntry] = Object.entries(validationEntry);
+    const [validation, validationRequirement] = ruleEntry;
+    const errorMessage = messageEntry?.[1] || `${validation} error`;
+
+    switch (validation) {
+      case 'required': {
+        if (validationRequirement && fieldValue.trim() === '') {
+          errors.push(errorMessage);
+        }
+        break;
+      }
+      case 'minLength': {
+        if (fieldValue.length < validationRequirement) {
+          errors.push(errorMessage);
+        }
+        break;
+      }
+      case 'maxLength': {
+        if (fieldValue.length > validationRequirement) {
+          errors.push(errorMessage);
+        }
+        break;
+      }
+      case 'email': {
+        const pattern = /^[a-zA-Z0-9]+@[a-z]+.[a-z]{2,5}$/;
+        if (fieldValue.match(pattern) === null) {
+          errors.push(errorMessage);
+        }
+        break;
+      }
+      default:
+    }
+  });
+
+  return errors;
+};
+
 export const FormContext = createContext();
 
 function FormProvider({ children }) {
