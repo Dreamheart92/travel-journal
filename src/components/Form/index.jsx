ca788cbeace4109e1c8a7c7271eebd23081f39d7
Toni Rachev <tonirachev92@gmail.com>
2024-07-27 04:35:14 +0300

    added useEffect to get the initial form value from children

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,8 +7,30 @@ export default function Form({ children }) {
   const { setFormInitialState, isValidForm } = useContext(FormContext);
   const initFormState = useRef(false);
 
+  useEffect(() => {
+    const childrenArray = Children.toArray(children);
 
-      {children}
+    const initialState = childrenArray.reduce((state, child) => {
+      if (child.type.name === 'Button') {
+        return state;
+      }
+
+      const fieldName = child.props.name;
+      const fieldValue = child.props?.initialValue || '';
+      const validators = child.props?.validators || [];
+
+      return {
+        ...state,
+        [fieldName]: {
+          state: { value: fieldValue, isDirty: false },
+          validators,
+        },
+      };
+    }, {});
+
+    setFormInitialState(initialState);
+    initFormState.current = true;
+  }, []);
     </form>
   );
 }
