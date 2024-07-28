368fa94c58622a3cfbf238bb6de449642266099a
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:17:34 +0300

    created form context reducer

---
+++ src/context/FormContext/reducer.jsx
@@ -0,0 +1,7 @@
+import * as actionTypes from './types';
+import { getErrorsAndValidateForm } from '../../helpers/validateForm';
+
+function formReducer(state, action) {
+}
+
+export default formReducer;
