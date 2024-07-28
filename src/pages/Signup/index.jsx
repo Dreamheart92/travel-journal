9ed964269a24cb3cdbc0f4457a123fe986dfd3e8
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:24:41 +0300

    removed form handlers

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,53 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import crudConstants from '../../constants/crudConstants';
+import { crudActions } from '../../store/crud/index';
+
 import AuthLayout from '../../layouts/AuthLayout';
 import SignupForm from '../../forms/SignupForm';
-import { constructSignupData } from '../../forms/helpers/signupForm';
-import crudConstants from '../../constants/crudConstants';
-import crudActionsConstants from '../../constants/crudActionsConstants';
-import { sendSignupRequest } from '../../store/crud/thunks';
-import { selectCreateState } from '../../store/crud/selectors';
-import { crudActions } from '../../store/crud';
-import { PATHS } from '../../constants/paths';
-import { storeUserData } from '../../helpers/storage';
+import FormProvider from '../../context/FormContext';
 
 export default function Signup() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const {
-    data: userData,
-    loading,
-    error,
-    success,
-  } = useSelector(selectCreateState);
-
-  useEffect(() => {
-    dispatch(crudActions.resetState({ key: crudConstants.CREATE }));
-
-    if (success) {
-      storeUserData(userData);
-      navigate(PATHS.HOME);
-    }
-  }, [success]);
-
-  const [passwordsNotMatching, setPasswordsNotMatching] = useState(false);
-
-  const handleSignupSubmit = (signupData) => {
-    setPasswordsNotMatching(false);
-
-    if (signupData.password !== signupData.confirmPassword) {
-      setPasswordsNotMatching(true);
-      return;
-    }
-
-    dispatch(sendSignupRequest({
-      key: crudConstants.CREATE,
-      currentAction: crudActionsConstants.SIGNUP,
-      signupData: constructSignupData(signupData),
-    }));
-  };
+  useEffect(() => () => dispatch(crudActions.resetState({ key: crudConstants.CREATE })));
 
   return (
     <AuthLayout
