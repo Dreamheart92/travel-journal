343c43a62ffc8670f4d72f9635fefc20a4bff26d
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 11:45:30 +0300

    added footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import style from './index.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={style.footer}>
+      <h5>SoftUni Project Defense 2024</h5>
+    </footer>
+  );
+}
