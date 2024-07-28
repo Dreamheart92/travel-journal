37c3131581d0c90d6a0061038b3889b115e897f0
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:05:33 +0300

    add consuming form context and rendering inputType conditionally

--- src/components/Form/Input/FormInput/index.jsx
+++ src/components/Form/Input/FormInput/index.jsx
@@ -1,3 +1,12 @@
+import TextInput from './TextInput';
+import ErrorMessage from '../../../ErrorMessage';
+import FileInput from './FileInput';
+import SelectInput from './SelectInput';
+import DateInput from './DateInput';
+import TextAreaInput from './TextAreaInput';
+import { useFormContext } from '../../../../context/FormContext';
+import style from '../index.module.css';
+
 export default function FormInput(
   {
     name,
@@ -9,6 +18,44 @@ export default function FormInput(
     options,
   },
 ) {
+  const { formContextActions, formContextSelectors } = useFormContext();
+
+  const formState = formContextSelectors.formState();
+  const isSubmitted = formContextSelectors.isSubmitted();
+  const { value, isDirty } = formState[name].state;
+
+  const errors = formContextSelectors.getFieldError(name);
+  const errorMessage = errors.length > 0 ? errors[0] : null;
+
+  const displayError = errorMessage && (isDirty || isSubmitted);
+
+  const inputClassName = `${style.field} ${displayError ? style.error : null}`;
+
+  const fieldProps = {
+    value,
+    onChange: formContextActions.onChange,
+    onBlur: formContextActions.onBlur,
+    inputClassName,
+    name,
+    placeholder,
+    type,
+    isJournal,
+    label,
+    options,
+  };
+
+  const Input = (inputType === 'text' && TextInput)
+    || (inputType === 'file' && FileInput)
+    || (inputType === 'select' && SelectInput)
+    || (inputType === 'date' && DateInput)
+    || (inputType === 'text-area' && TextAreaInput);
+
   return (
+    <div>
+      <Input fieldProps={fieldProps} />
+
+      {displayError
+        && <ErrorMessage message={errorMessage} />}
+    </div>
   );
 }
