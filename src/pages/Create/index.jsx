69786ed928f0f1223cdbaa1061e4eb0c67b668af
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:23:31 +0300

    wrapped form with form context

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,50 +1,53 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { buildJournalFormData } from '../../helpers';
 import JournalEditor from '../../components/JournalEditor';
-import { PATHS } from '../../constants/paths';
-import { postJournalRequest } from '../../store/crud/thunks';
-import crudConstants from '../../constants/crudConstants';
-import crudActionsConstants from '../../constants/crudActionsConstants';
-import { selectCreateState } from '../../store/crud/selectors';
 import { crudActions } from '../../store/crud';
+import crudConstants from '../../constants/crudConstants';
+import FormProvider from '../../context/FormContext';
+import { selectCreateState } from '../../store/crud/selectors';
+import { postJournalRequest } from '../../store/crud/thunks';
+import crudActionsConstants from '../../constants/crudActionsConstants';
+import { buildJournalFormData } from '../../helpers';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '../../constants/paths';
 
 export default function Create() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const {
-    data: newJournalEntry,
+    data: journalData,
     loading,
     success,
-    error,
+    error
   } = useSelector(selectCreateState);
 
   useEffect(() => {
     dispatch(crudActions.resetState({ key: crudConstants.CREATE }));
-
     if (success) {
-      navigate(`${PATHS.DETAILS}/${newJournalEntry._id}`);
+      navigate(`${PATHS.DETAILS}/${journalData._id}`);
     }
   }, [success]);
 
-  const handleCreateSubmit = (journalData) => {
+  const handleCreateJournalSubmit = (formData) => {
     dispatch(postJournalRequest({
       key: crudConstants.CREATE,
       currentAction: crudActionsConstants.POST_JOURNAL,
-      journalData: buildJournalFormData(journalData),
+      journalData: buildJournalFormData(formData),
     }));
   };
 
   return (
-    <JournalEditor
-      title="Share your journey!"
-      caption="Create your travel journal and capture every moment of your adventure. Share your stories,
+    <FormProvider>
+      <JournalEditor
+        title="Share your journey!"
+        caption="Create your travel journal and capture every moment of your adventure. Share your stories,
                 photos, and experiences with the world."
-      type="Create"
-      submitCallback={handleCreateSubmit}
-      isSubmitting={loading}
-    />
+        buttonCaption="Create"
+        submitCallback={handleCreateJournalSubmit}
+        isSubmitting={loading}
+        requestError={error}
+      />
+    </FormProvider>
   );
 }
