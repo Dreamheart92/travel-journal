4a1c64b9afda55251a4ec6474383e819fc32b5f4
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:20:56 +0300

    refactor form

--- src/forms/SignupForm/index.jsx
+++ src/forms/SignupForm/index.jsx
@@ -1,143 +1,185 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import useForm from '../../hooks/useForm';
+
+import { PATHS } from '../../constants/paths';
 import VALIDATIONS from '../../constants/validations';
+import crudConstants from '../../constants/crudConstants';
+
+import crudActionsConstants from '../../constants/crudActionsConstants';
+
+import { sendSignupRequest } from '../../store/crud/thunks';
+import { constructSignupData } from '../helpers/signupForm';
+
+import { selectCreateState } from '../../store/crud/selectors';
+
+import { deleteUserDataFromStorage, storeUserData } from '../../helpers/storage';
+
 import Form from '../../components/Form';
-import TextInput from '../../components/Input/TextInput';
 import Button from '../../components/Button';
-import { resetPasswordFields } from '../helpers/signupForm';
-import ErrorMessage from '../../components/ErrorMessage';
 
-export default function SignupForm({ onSignupSubmit, isSubmitting, error, passwordsNotMatching }) {
-  const {
-    register,
-    handleSubmit,
-    clearFieldValue,
-  } = useForm({ submitCallback: onSignupSubmit });
+export default function SignupForm() {
+  const [form] = useForm();
 
-  const { handlers: emailHandlers, state: emailState } = register('email', '', {
-    required: true,
-    email: true,
-    minLength: VALIDATIONS.USER.EMAIL_MIN_LENGTH,
-  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const {
-    handlers: usernameHandlers,
-    state: usernameState,
-  } = register(
-    'username',
-    '',
-    {
-      required: true,
-      minLength: VALIDATIONS.USER.USERNAME_MIN_LENGTH,
-    },
-  );
-
-  const {
-    handlers: firstNameHandlers,
-    state: firstNameState,
-  } = register(
-    'firstName',
-    '',
-    {
-      required: true,
-      minLength: VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH,
-    },
-  );
-
-  const {
-    handlers: lastNameHandlers,
-    state: lastNameState,
-  } = register(
-    'lastName',
-    '',
-    {
-      required: true,
-      minLength: VALIDATIONS.USER.LAST_NAME_MIN_LENGTH,
-    },
-  );
-
-  const {
-    handlers: passwordHandlers,
-    state: passwordState,
-  } = register(
-    'password',
-    '',
-    {
-      required: true,
-      minLength: VALIDATIONS.USER.PASSWORD_MIN_LENGTH,
-    },
-  );
-
-  const {
-    handlers: confirmPasswordHandlers,
-    state: confirmPasswordState,
-  } = register(
-    'confirmPassword',
-    '',
-    {
-      required: true,
-      minLength: VALIDATIONS.USER.PASSWORD_MIN_LENGTH,
-    },
-  );
+    data: userData,
+    loading,
+    success,
+    error,
+  } = useSelector(selectCreateState);
 
   useEffect(() => {
-    if (error || passwordsNotMatching) {
-      resetPasswordFields(clearFieldValue);
+    if (success) {
+      deleteUserDataFromStorage();
+      storeUserData(userData);
+      navigate(PATHS.HOME);
     }
-  }, [error, passwordsNotMatching]);
+
+    if (error) {
+      form.resetField('password');
+      form.resetField('confirmPassword');
+    }
+  }, [success, error]);
+
+  const [passwordsMatching, setPasswordsMatching] = useState(true);
+
+  const handleSignupSubmit = (signupData) => {
+    const { password, confirmPassword } = signupData;
+
+    if (password !== confirmPassword) {
+      setPasswordsMatching(false);
+      form.resetField('password');
+      form.resetField('confirmPassword');
+      return;
+    }
+
+    setPasswordsMatching(true);
+
+    dispatch(sendSignupRequest({
+      key: crudConstants.CREATE,
+      currentAction: crudActionsConstants.SIGNUP,
+      signupData: constructSignupData(signupData),
+    }));
+  };
+
+  const requestError = !passwordsMatching
+    ? { error: true, message: 'Passwords not matching' }
+    : error;
+
+  const disableSubmitButton = (!form.isValidForm || !passwordsMatching) && form.hasBeenSubmitted;
 
   return (
     <Form
-      isSubmitting={isSubmitting}
-      onSubmit={handleSubmit}
-      error={error}
+      submitCallback={handleSignupSubmit}
+      error={requestError}
     >
-      <TextInput
-        handlers={emailHandlers}
-        state={emailState}
+      <Form.Input
+        name="email"
         placeholder="Email"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'Email is required',
+          },
+          {
+            email: true,
+            message: 'Email address is invalid',
+          },
+        ]}
       />
 
-      <TextInput
-        handlers={usernameHandlers}
-        state={usernameState}
+      <Form.Input
+        name="username"
         placeholder="Username"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'Username is required',
+          },
+          {
+            minLength: VALIDATIONS.USER.USERNAME_MIN_LENGTH,
+            message: `Username must be at least ${VALIDATIONS.USER.USERNAME_MIN_LENGTH} characters long`,
+          },
+        ]}
       />
 
-      <TextInput
-        handlers={firstNameHandlers}
-        state={firstNameState}
-        placeholder="First name"
+      <Form.Input
+        name="firstName"
+        placeholder="First Name"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'First Name is required',
+          },
+          {
+            minLength: VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH,
+            message: `First Name must be at least ${VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH} characters long`,
+          },
+        ]}
       />
 
-      <TextInput
-        handlers={lastNameHandlers}
-        state={lastNameState}
-        placeholder="Last name"
+      <Form.Input
+        name="lastName"
+        placeholder="Last Name"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'Last Name is required',
+          },
+          {
+            minLength: VALIDATIONS.USER.LAST_NAME_MIN_LENGTH,
+            message: `Last Name must be at least ${VALIDATIONS.USER.LAST_NAME_MIN_LENGTH} characters long`,
+          },
+        ]}
       />
 
-      <TextInput
-        handlers={passwordHandlers}
-        state={passwordState}
-        type="password"
+      <Form.Input
+        name="password"
         placeholder="Password"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'Password is required',
+          },
+          {
+            minLength: VALIDATIONS.USER.PASSWORD_MIN_LENGTH,
+            message: `Password must be at least ${VALIDATIONS.USER.PASSWORD_MIN_LENGTH} characters long`,
+          },
+        ]}
       />
 
-      <TextInput
-        handlers={confirmPasswordHandlers}
-        state={confirmPasswordState}
-        type="password"
-        placeholder="Confirm password"
+      <Form.Input
+        name="confirmPassword"
+        placeholder="Confirm Password"
+        inputType="text"
+        validators={[
+          {
+            required: true,
+            message: 'Confirm Password is required',
+          },
+          {
+            minLength: VALIDATIONS.USER.PASSWORD_MIN_LENGTH,
+            message: `Confirm Password must be at least ${VALIDATIONS.USER.PASSWORD_MIN_LENGTH} characters long`,
+          },
+        ]}
       />
-
-      {passwordsNotMatching
-        && <ErrorMessage message="Passwords not matching" />}
 
       <Button
-        caption="Signup"
         submitButton
-        isLoading={isSubmitting}
+        caption="Sign up"
+        disabled={disableSubmitButton}
+        isLoading={loading}
       />
+
     </Form>
   );
 }
