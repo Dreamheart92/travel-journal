00c9c4e2f43d2c0eadceb5916dcb45c7f0f15f9a
Toni Rachev <tonirachev92@gmail.com>
2024-07-28 12:17:51 +0300

    add init form state reducer

--- src/context/FormContext/reducer.jsx
+++ src/context/FormContext/reducer.jsx
@@ -2,6 +2,18 @@ import * as actionTypes from './types';
 import { getErrorsAndValidateForm } from '../../helpers/validateForm';
 
 function formReducer(state, action) {
+  switch (action.type) {
+    case actionTypes.INIT_FORM_STATE: {
+      const { initialState } = action.payload;
+
+      const { errors } = getErrorsAndValidateForm(initialState);
+
+      return {
+        ...state,
+        formState: initialState,
+        errors,
+      };
+    }
 }
 
 export default formReducer;
