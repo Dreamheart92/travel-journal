cd2fd4430451f6d48d8b0cacb4ac89a420d51e6d
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:05:23 +0300

    refactor keys

--- src/store/entries/selectors.js
+++ src/store/entries/selectors.js
@@ -1,16 +1,16 @@
 import { createSelector } from '@reduxjs/toolkit';
-import entriesConstants from '../../constants/entriesConstants';
+import entriesKeys from './types';
 
 const selectEntriesSlice = (state) => state.entries;
 
-export const selectJournalsEntries = (state) => state.entries[entriesConstants.JOURNAL_ENTRIES];
+export const selectJournalsEntries = (state) => state.entries[entriesKeys.JOURNAL_ENTRIES];
 
-export const selectJournalEntry = (state) => state.entries[entriesConstants.JOURNAL_ENTRY];
+export const selectJournalEntry = (state) => state.entries[entriesKeys.JOURNAL_ENTRY];
 
 export const selectComments = createSelector(
   [selectEntriesSlice, selectJournalEntry],
   (entriesSlice, journalEntry) => ({
-    comments: entriesSlice[entriesConstants.COMMENTS],
+    comments: entriesSlice[entriesKeys.COMMENTS],
     loading: journalEntry.loading,
   }),
 );
