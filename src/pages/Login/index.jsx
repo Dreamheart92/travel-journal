b598f3efa5ca7b4116af88b7b116d76c5dceb445
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 15:02:09 +0300

    add login settings

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import LoginForm from '../../forms/LoginForm';
-import AuthLayout from '../../layouts/AuthLayout';
+import LoginForm from '../../modules/AuthModule/Forms/LoginForm';
 import { crudActions } from '../../store/crud';
 import crudKeys from '../../store/crud/types';
-import FormProvider from '../../context/FormContext';
+import AuthModule from '../../modules/AuthModule';
+
+const settings = {
+  title: 'Welcome Back, Explorer!',
+  caption: 'Log in to your Travel Journal and continue your journey of sharing adventures and discovering new destinations.',
+  form: <LoginForm />,
+};
 
 export default function Login() {
   const dispatch = useDispatch();
