dda8cbf3fa27066f8d4a3fe1540af08f2f078503
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:47:41 +0300

    add handleStoreUser func

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,6 +3,14 @@ import { authActions } from '../store/auth';
 import { deleteUserDataFromLocalStorage, saveUserDataToLocalStorage } from '../helpers/storage';
 
 const useAuth = () => {
+  const dispatch = useDispatch();
+
+  const handleStoreUser = (userData) => {
+    saveUserDataToLocalStorage(userData);
+    dispatch(authActions.storeUser({ userData }));
+  };
+
+    saveUser: handleStoreUser,
 };
 
 export default useAuth;
