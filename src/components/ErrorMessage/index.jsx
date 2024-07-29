11b2270fa9cd39a11b929475bfc775d874121e41
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:30:44 +0300

    add is refresh error

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -1,7 +1,33 @@
 import style from './index.module.css';
 
-export default function ErrorMessage({ message }) {
+export default function ErrorMessage({ large = false, isRefreshError = false, message }) {
+  const errorClass = `${style['error-message']} ${large ? style.large : style.small}`;
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  if (isRefreshError) {
+    return (
+      <div className={style['error-container']}>
+        <p className={errorClass}>{message}</p>
+        <p className={errorClass}>Please </p>
+
+        <p
+          onClick={handleRefresh}
+          className={`${errorClass} ${style.refresh}`}
+        >
+          refresh
+        </p>
+
+        <p className={errorClass}>or try again later.</p>
+      </div>
+    );
+  }
+
   return (
-    <p className={style['error-message']}>{message}</p>
+    <p className={errorClass}>
+      {message}
+    </p>
   );
 }
