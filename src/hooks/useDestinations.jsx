2dc09be44e8626c79bd41bf0659c3abdb747633e
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:38:08 +0300

    add dispatch and consuming destinations state

--- src/hooks/useDestinations.jsx
+++ src/hooks/useDestinations.jsx
@@ -3,6 +3,14 @@ import { selectDestinations } from '../store/destinations/selectors';
 import { fetchDestinations } from '../store/destinations/services';
 
 const useDestinations = () => {
+  const dispatch = useDispatch();
+
+  const {
+    destinations,
+    error,
+    loading,
+    success,
+  } = useSelector(selectDestinations);
 };
 
 export default useDestinations;
