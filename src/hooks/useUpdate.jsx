cb14d7e3db112182212dfef6932722225828ecad
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 21:20:12 +0300

    add handle update profile request

--- src/hooks/useUpdate.jsx
+++ src/hooks/useUpdate.jsx
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUpdateState } from '../store/crud/selectors';
+import { updateJournalRequest, updateProfileRequest } from '../store/crud/services';
+import crudKeys from '../store/crud/types';
+import crudActionsConstants from '../constants/crudActionsConstants';
+import { buildJournalFormData, buildUserFormData } from '../helpers';
 
 const useUpdate = () => {
   const dispatch = useDispatch();
@@ -11,11 +15,20 @@ const useUpdate = () => {
     error,
   } = useSelector(selectUpdateState);
 
+  const handleUpdateProfileRequest = (userData) => {
+    dispatch(updateProfileRequest({
+      key: crudKeys.UPDATE,
+      currentAction: crudActionsConstants.UPDATE_PROFILE,
+      userData: buildUserFormData(userData),
+    }));
+  };
+
   return {
     data,
     loading,
     success,
     error,
+    updateProfile: handleUpdateProfileRequest,
   };
 };
 
