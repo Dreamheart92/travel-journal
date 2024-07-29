710d8651bc54ec5cd22307cb6a0d20458c4f6d77
Toni Rachev <tonirachev92@gmail.com>
2024-07-30 00:22:57 +0300

    refactor replace useSelector with useJournal hook

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,32 +1,32 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Navigate, useParams } from 'react-router-dom';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import Container from '../../components/Container';
-import { selectJournalEntry } from '../../store/entries/selectors';
 import Loading from '../../components/Loading';
-import { fetchEntry } from '../../store/entries/services';
 import { entriesActions } from '../../store/entries';
 import entriesKeys from '../../store/entries/types';
 import ErrorMessage from '../../components/ErrorMessage';
 import { PATHS } from '../../constants/paths';
 import JournalModule from '../../modules/DetailsModule/JournalModule';
 import CommentsModule from '../../modules/DetailsModule/CommentsModule';
+import useJournal from '../../hooks/useJournal';
 
 export default function Details() {
   const { journalId } = useParams();
 
   const {
-    result: journal,
+    journal,
     loading,
     success,
     error,
-  } = useSelector(selectJournalEntry);
+    fetchJournal,
+  } = useJournal();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const promise = dispatch(fetchEntry({ journalId }));
+    const promise = fetchJournal(journalId);
 
     return () => {
       dispatch(entriesActions.resetState({ key: entriesKeys.JOURNAL_ENTRY }));
