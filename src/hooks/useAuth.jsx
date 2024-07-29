c52df88401acb1338c59111b6c8ea3fd08b7d7ff
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:47:23 +0300

    created use auth hook

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,8 @@
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
+import { deleteUserDataFromLocalStorage, saveUserDataToLocalStorage } from '../helpers/storage';
+
+const useAuth = () => {
+};
+
+export default useAuth;
