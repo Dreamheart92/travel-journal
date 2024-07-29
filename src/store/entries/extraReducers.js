82a4cf0159918ee7a2d28221d4a850be090e42c6
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:01:57 +0300

    moved to extraReducers

---
+++ src/store/entries/extraReducers.js
@@ -0,0 +1,60 @@
+import { fetchEntry } from './services';
+import entriesKeys from './types';
+import { buildErrorObject } from '../../helpers';
+
+const extraReducers = (builder) => {
+  builder.addCase(fetchEntry.pending, (state) => {
+    state[entriesKeys.JOURNAL_ENTRY].loading = true;
+    state[entriesKeys.JOURNAL_ENTRY].success = false;
+    state[entriesKeys.JOURNAL_ENTRY].error = null;
+  });
+  builder.addCase(fetchEntry.fulfilled, (state, action) => {
+    state[entriesKeys.JOURNAL_ENTRY].result = action.payload.result;
+    state[entriesKeys.JOURNAL_ENTRY].isJournalOwner = action.payload.isJournalOwner;
+    state[entriesKeys.COMMENTS].results = action.payload.comments;
+
+    state[entriesKeys.JOURNAL_ENTRY].loading = false;
+    state[entriesKeys.JOURNAL_ENTRY].success = true;
+  });
+  builder.addCase(fetchEntry.rejected, (state, action) => {
+    if (action.error.message !== 'Aborted') {
+      state[entriesKeys.JOURNAL_ENTRY].loading = false;
+      state[entriesKeys.JOURNAL_ENTRY].error = buildErrorObject(action.error.message);
+    }
+  });
+  builder.addMatcher(
+    (action) => action.type.startsWith('entries/fetchEntries'),
+    (state, action) => {
+      const { requestStatus } = action.meta;
+
+      switch (requestStatus) {
+        case 'pending': {
+          state[entriesKeys.JOURNAL_ENTRIES].loading = true;
+          state[entriesKeys.JOURNAL_ENTRIES].success = false;
+          state[entriesKeys.JOURNAL_ENTRIES].error = null;
+          break;
+        }
+        case 'fulfilled': {
+          state[entriesKeys.JOURNAL_ENTRIES].results = {
+            journals: action.payload.journals,
+            totalPages: action.payload.totalPages,
+          };
+
+          state[entriesKeys.JOURNAL_ENTRIES].loading = false;
+          state[entriesKeys.JOURNAL_ENTRIES].success = true;
+          break;
+        }
+        case 'rejected': {
+          if (action.error.message !== 'Aborted') {
+            state[entriesKeys.JOURNAL_ENTRIES].error = buildErrorObject('Failed to fetch journals. Please try again.');
+            state[entriesKeys.JOURNAL_ENTRIES].loading = false;
+          }
+          break;
+        }
+        default:
+      }
+    },
+  );
+};
+
+export default extraReducers;
