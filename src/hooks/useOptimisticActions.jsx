71f5123736257750537c2c9ab12438e05570a15e
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:27:12 +0300

    add handle post comment func

--- src/hooks/useOptimisticActions.jsx
+++ src/hooks/useOptimisticActions.jsx
@@ -3,5 +3,20 @@ import { deleteCommentRequest, postCommentReactionRequest, postCommentRequest } 
 import optimisticKeys from '../store/optimistic/types';
 const useOptimisticActions = () => {
   const dispatch = useDispatch();
+
+  const handlePostComment = (localComment, journalId) => {
+    dispatch(postCommentRequest({
+      key: optimisticKeys.POST_COMMENT,
+      commentMetaData: {
+        commentData: {
+          comment: localComment.comment,
+          createdAt: localComment.createdAt,
+        },
+        journalId,
+      },
+    }));
+  };
+    postCommentOptimistic: handlePostComment,
 };
+
 export default useOptimisticActions;
