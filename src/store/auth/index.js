a19a6b7514d47670eea3c0f41552ad4bd8fffc1b
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:58:36 +0300

    add get initial auth state

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,13 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
-  user: null,
-  isAuthenticated: false,
-};
+import { storeUser, clearUser } from './reducers';
+import { getInitialAuthState } from '../../helpers/getInitialAuthState';
 
 const authSlice = createSlice({
   name: 'auth',
-  initialState,
+  initialState: getInitialAuthState,
   reducers: {
     storeUser(state, action) {
       state.user = action.payload.userData;
