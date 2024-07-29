7ccdd33b5618d714aa3e544e7e6363e663e83637
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 14:26:15 +0300

    added sidebar with navigation links

--- src/modules/AccountModule/AccountSidebarModule/index.jsx
+++ src/modules/AccountModule/AccountSidebarModule/index.jsx
@@ -1,5 +1,27 @@
+import SidebarSection from '../../../components/Sidebar/SidebarSection';
+import NavigationLink from '../../../components/NavigationLink';
+import { ACCOUNT_SUB_PATHS, PATHS } from '../../../constants/paths';
+import Sidebar from '../../../components/Sidebar';
+
 export default function AccountSidebarModule() {
   return (
-    
-  )
+    <Sidebar width="fit-content">
+      <SidebarSection heading="My Account">
+
+        <NavigationLink to={PATHS.ACCOUNT} end>
+          My Account
+        </NavigationLink>
+
+        <NavigationLink to={ACCOUNT_SUB_PATHS.EDIT_PROFILE}>
+          Edit Profile
+        </NavigationLink>
+      </SidebarSection>
+
+      <SidebarSection heading="Journals">
+        <NavigationLink to={ACCOUNT_SUB_PATHS.MY_JOURNALS}>
+          My Journals
+        </NavigationLink>
+      </SidebarSection>
+    </Sidebar>
+  );
 }
