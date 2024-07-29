11cc2cb883ed46c9ec92716fa94b6295a6ba87e2
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:37:52 +0300

    created useDestinations hook

---
+++ src/hooks/useDestinations.jsx
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDestinations } from '../store/destinations/selectors';
+import { fetchDestinations } from '../store/destinations/services';
+
+const useDestinations = () => {
+};
+
+export default useDestinations;
