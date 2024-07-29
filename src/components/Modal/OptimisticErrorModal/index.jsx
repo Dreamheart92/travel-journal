41d2c829790dce82c8152c9b5ec471749d1bc718
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:33:41 +0300

    created optimistic error modal

---
+++ src/components/Modal/OptimisticErrorModal/index.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import ErrorMessage from '../../ErrorMessage';
+import useModal from '../../../hooks/useModal';
+import Modal from '../index';
+import { selectOptimisticErrors } from '../../../store/optimistic/selectors';
+import { optimisticErrorMessages } from '../../../constants/errorMessages';
+
+export default function OptimisticErrorModal() {
+  const { isOpen: isModalOpen, onOpenModal } = useModal();
+  return (
+    <Modal isOpen={isModalOpen}>
+    </Modal>
+  );
+}
