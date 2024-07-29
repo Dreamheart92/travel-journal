5ec71ba96f6d25440295a6c3f3e41ab34d29274b
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:53:48 +0300

    created CatalogueSidebarModule

---
+++ src/modules/CatalogueModule/CatalogueSidebarModule/index.jsx
@@ -0,0 +1,9 @@
+import Sidebar from '../../../components/Sidebar';
+import CreateJournal from '../../../components/Sidebar/CreateJournal';
+import Search from '../../../components/Search';
+import FiltersSection from '../../../components/Sidebar/FiltersSection';
+
+export default function CatalogueSidebarModule({ isAuthenticated, onQuery }) {
+  return (
+  );
+}
