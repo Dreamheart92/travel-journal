bd9cc87750042baa6d63c4240c8922eff22d25d0
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 13:18:35 +0300

    renamed latest journals and moved to modules

--- src/modules/HomeModule/LatestJournalsModule/index.jsx
+++ src/modules/HomeModule/LatestJournalsModule/index.jsx
@@ -1,18 +1,24 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Container from '../Container';
-import Grid from '../Grid';
-import HomeCard from '../HomeCard';
-import Loading from '../Loading';
-import { fetchEntries } from '../../store/entries/services';
-import { selectJournalsEntries } from '../../store/entries/selectors';
-import { entriesActions } from '../../store/entries';
-import entriesKeys from '../../store/entries/types';
-import ErrorMessage from '../ErrorMessage';
+import Container from '../../../components/Container';
+import Grid from '../../../components/Grid';
+import HomeCard from '../../../components/HomeCard';
+import Loading from '../../../components/Loading';
+import { fetchEntries } from '../../../store/entries/services';
+import { selectJournalsEntries } from '../../../store/entries/selectors';
+import { entriesActions } from '../../../store/entries';
+import entriesKeys from '../../../store/entries/types';
+import ErrorMessage from '../../../components/ErrorMessage';
 
-export default function LatestJournals() {
+export default function LatestJournalsModule() {
   const dispatch = useDispatch();
-  const { results, loading, error, success } = useSelector(selectJournalsEntries);
+
+  const {
+    results,
+    loading,
+    error,
+    success,
+  } = useSelector(selectJournalsEntries);
 
   useEffect(() => {
     dispatch(entriesActions.resetState({ key: entriesKeys.JOURNAL_ENTRIES }));
