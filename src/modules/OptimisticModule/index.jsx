adaa4afa4e56dd8fea2422f5518239917536b3e3
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:31:40 +0300

    add useEffect to handle optimistic errors

--- src/modules/OptimisticModule/index.jsx
+++ src/modules/OptimisticModule/index.jsx
@@ -30,6 +30,20 @@ export default function OptimisticModule() {
       dispatch(entriesActions.updateLocalCommentWithRealData(postCommentSuccessData));
     }
   }, [postCommentSuccessData]);
+
+  useEffect(() => {
+    const optimisticErrorsEntries = Object.entries(optimisticErrors);
+
+    for (let i = 0; i < optimisticErrorsEntries.length; i += 1) {
+      const [key, error] = optimisticErrorsEntries[i];
+
+      if (error) {
+        setErrorMessage(optimisticErrorMessages[key]);
+        onOpenModal();
+        break;
+      }
+    }
+  }, [optimisticErrors]);
     <Modal isOpen={isModalOpen}>
     </Modal>
   );
