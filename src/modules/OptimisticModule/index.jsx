9a0b09fb4d8a54ad6b5823ba1f96ec397a659089
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:30:31 +0300

    computing optimisticErrors

--- src/modules/OptimisticModule/index.jsx
+++ src/modules/OptimisticModule/index.jsx
@@ -18,6 +18,12 @@ export default function OptimisticModule() {
   const optimisticState = useSelector(selectOptimistic);
 
   const postCommentSuccessData = optimisticState[optimisticKeys.POST_COMMENT].data;
+
+  const optimisticErrors = {
+    [optimisticKeys.POST_COMMENT]: optimisticState[optimisticKeys.POST_COMMENT].error,
+    [optimisticKeys.DELETE_COMMENT]: optimisticState[optimisticKeys.DELETE_COMMENT].error,
+    [optimisticKeys.POST_COMMENT_REACTION]: optimisticState[optimisticKeys.POST_COMMENT_REACTION].error,
+  };
     <Modal isOpen={isModalOpen}>
     </Modal>
   );
