4623db56205947782f75a05c2df24860289a740e
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:28:21 +0300

    created optimistic module

---
+++ src/modules/OptimisticModule/index.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectOptimistic } from '../../store/optimistic/selectors';
+import useModal from '../../hooks/useModal';
+import { optimisticErrorMessages } from '../../constants/errorMessages';
+import Modal from '../../components/Modal';
+import ErrorMessage from '../../components/ErrorMessage';
+import optimisticKeys from '../../store/optimistic/types';
+import { entriesActions } from '../../store/entries';
+
+export default function OptimisticModule() {
+  );
+}
