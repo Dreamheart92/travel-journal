b3036c02579c7dbf1e3cc22b652f1205362eb11c
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 18:04:26 +0300

    add props and buidling the form initial state

--- src/modules/JournalEditorModule/index.jsx
+++ src/modules/JournalEditorModule/index.jsx
@@ -1,5 +1,21 @@
+import JournalEditorLayout from '../../layouts/JournalEditorLayout';
+import FormProvider from '../../context/FormContext';
+import JournalForm from '../../forms/JournalForm';
+import { buildFormInitialState } from '../../helpers';
 
 export default function JournalEditorModule({ settings }) {
+  const {
+    title,
+    caption,
+    buttonCaption,
+    isSubmitting,
+    submitCallback,
+    requestError,
+    journal,
+  } = settings;
+
+  const formInitialState = buildFormInitialState(journal);
+
   return (
   );
 }
