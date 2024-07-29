b96179ba890d93de1800546731c2d5acdf13a29e
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:25:16 +0300

    removed initApp state

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import Navigation from './components/Navigation';
 import Loading from './components/Loading';
 import Footer from './components/Footer';
-import OptimisticErrorModal from './components/Modal/OptimisticErrorModal';
 import useDestinations from './hooks/useDestinations';
 
 export default function App() {
-  const [initApp, setInitApp] = useState(false);
-
   const { success: destinationsLoaded, fetchDestinations } = useDestinations();
 
   useEffect(() => {
     fetchDestinations();
-    setInitApp(true);
   }, []);
 
-  const appIsLoaded = initApp && destinationsLoaded;
-
-  if (!appIsLoaded) {
+  if (!destinationsLoaded) {
     return <Loading />;
   }
 
