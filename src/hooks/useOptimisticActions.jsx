8aa1c821bad086ec913ad4bd140bd6ad0fa114a5
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 20:27:41 +0300

    Update useOptimisticActions.jsx

--- src/hooks/useOptimisticActions.jsx
+++ src/hooks/useOptimisticActions.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { deleteCommentRequest, postCommentReactionRequest, postCommentRequest } from '../store/optimistic/services';
 import optimisticKeys from '../store/optimistic/types';
+
 const useOptimisticActions = () => {
   const dispatch = useDispatch();
 
