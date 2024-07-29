dc5c20416586cd1b9cce1247e69928fa0f4566a6
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:31:44 +0300

    refactor loading state and rendering error

--- src/components/JournalsList/index.jsx
+++ src/components/JournalsList/index.jsx
@@ -2,41 +2,65 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import CatalogueCard from '../CatalogueCard';
 import Loading from '../Loading';
-import { fetchEntries } from '../../store/entries/thunks';
+import { fetchEntries } from '../../store/entries/services';
 import { selectJournalsEntries } from '../../store/entries/selectors';
 import { entriesActions } from '../../store/entries';
-import entriesConstants from '../../constants/entriesConstants';
+import entriesKeys from '../../store/entries/types';
+import { buildQueryString } from '../../helpers';
+import { searchActions } from '../../store/search';
+import ErrorMessage from '../ErrorMessage';
+import Pagination from '../Pagination';
 
-export default function JournalsList({ destination, searchParams }) {
+export default function JournalsList({ destination, searchParams, onQuery }) {
   const dispatch = useDispatch();
 
   const {
     results,
     loading,
+    success,
+    error,
   } = useSelector(selectJournalsEntries);
 
+  const currentPage = Number(searchParams.get('page')) || 1;
+
   useEffect(() => {
-    dispatch(entriesActions.resetState({ key: entriesConstants.JOURNAL_ENTRIES }));
-    const search = searchParams.get('search');
-    const promise = dispatch(fetchEntries({ query: { search, destination } }));
+    dispatch(entriesActions.resetState({ key: entriesKeys.JOURNAL_ENTRIES }));
+
+    const query = buildQueryString(searchParams);
+    const promise = dispatch(fetchEntries({ query, destination }));
 
     return () => {
       promise.abort();
     };
   }, [dispatch, destination, searchParams]);
 
-  if (loading || !results) {
-    return <Loading />;
-  }
+  useEffect(() => {
+    dispatch(searchActions.resetState());
+  }, [destination]);
 
   return (
     <>
-      {results.journals.map((journal) => (
-        <CatalogueCard
-          key={journal._id}
-          journal={journal}
-        />
-      ))}
+      {(loading || (!success && !error))
+        && <Loading />}
+
+      {!loading && results
+        && (
+          <Pagination
+            onQuery={onQuery}
+            totalPages={results.totalPages}
+            currentPage={currentPage}
+          >
+            {results.journals.map((journal) => (
+              <CatalogueCard
+                key={journal._id}
+                journal={journal}
+              />
+            ))}
+          </Pagination>
+        )}
+
+      {error
+        && <ErrorMessage large message={error.message} />}
     </>
   );
 }
