5fcabced681360e76497d0442d19060277baa7fb
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 12:34:22 +0300

    add use effect on optimistic error and rendering the correct message

--- src/components/Modal/OptimisticErrorModal/index.jsx
+++ src/components/Modal/OptimisticErrorModal/index.jsx
@@ -13,6 +13,20 @@ export default function OptimisticErrorModal() {
 
   const optimisticErrors = useSelector(selectOptimisticErrors);
 
+  useEffect(() => {
+    const optimisticErrorsEntries = Object.entries(optimisticErrors);
+
+    for (let i = 0; i < optimisticErrorsEntries.length; i += 1) {
+      const [key, error] = optimisticErrorsEntries[i];
+
+      if (error) {
+        setErrorMessage(optimisticErrorMessages[key]);
+        onOpenModal();
+        break;
+      }
+    }
+  }, [optimisticErrors]);
+
   return (
     <Modal isOpen={isModalOpen}>
     </Modal>
