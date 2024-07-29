249d47ab3b44b7f318bede96526b956c99b53ba6
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 18:03:02 +0300

    rendering info block

--- src/layouts/JournalEditorLayout/index.jsx
+++ src/layouts/JournalEditorLayout/index.jsx
@@ -4,6 +4,11 @@ import Container from '../../components/Container';
 export default function JournalEditorLayout({ title, caption, children }) {
   return (
     <Container customStyle={{ paddingTop: '5em' }}>
+      <InfoBlock
+        title={title}
+        caption={caption}
+        paddingBottom="2em"
+      />
       {children}
     </Container>
   );
