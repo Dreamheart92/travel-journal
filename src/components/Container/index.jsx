090ce7518dba4e0411b90c88d45a3e27f22453cc
Toni Rachev <tonirachev92@gmail.com>
2024-07-29 18:01:10 +0300

    removed width and direction props

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -3,14 +3,11 @@ import style from './index.module.css';
 export default function Container(
   {
     children,
-    width = 'fitContent',
     heading,
-    direction = 'column',
   },
 ) {
   return (
     <div
-      style={{ width, flexDirection: direction }}
       className={style.container}
     >
       {heading
