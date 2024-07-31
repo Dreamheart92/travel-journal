07f03b28085e031801bff552284c333a634da1d9
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:20:53 +0300

    add no journal results messages

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const NO_JOURNAL_RESULTS = {
+  catalogue: 'Looks like your search hit a dead end! Don\'t worry, the journey isn\'t over. Try new keywords or dive into our latest epic travel escapades!',
+  account: 'You haven\'t started your travel journal yet. Begin your adventure by creating your first journal today!',
+};
