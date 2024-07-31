5441e2fa2b339db07618de5e3243a164d00caaac
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:19:45 +0300

    created no journal results component

---
+++ src/components/NoJournalsResults/index.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import { NO_JOURNAL_RESULTS } from '../../constants/constants';
+import { PATHS } from '../../constants/paths';
+import Button from '../Button';
+import style from './index.module.css';
+
+export default function NoJournalResults({ contextType = 'catalogue' }) {
+  );
+}
