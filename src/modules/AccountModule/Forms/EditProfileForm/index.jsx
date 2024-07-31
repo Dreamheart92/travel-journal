658aabc74f61f64c77de1afb944d1eca10e7ba24
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:25:02 +0300

    refactor wrapped inputs

--- src/modules/AccountModule/Forms/EditProfileForm/index.jsx
+++ src/modules/AccountModule/Forms/EditProfileForm/index.jsx
@@ -1,89 +1,107 @@
 import Form from '../../../../components/Form';
 import Button from '../../../../components/Button';
 import VALIDATIONS from '../../../../constants/validations';
+import style from './index.module.css';
 
-export default function EditProfileForm({ initialState, submitCallback, isSubmitting }) {
+export default function EditProfileForm(
+  {
+    initialState,
+    submitCallback,
+    isSubmitting,
+    error,
+  },
+) {
   return (
-    <Form submitCallback={submitCallback}>
-      <Form.Input
-        name="image"
-        initialValue={initialState.image}
-        inputType="file"
-        validators={[
-          {
-            required: true,
-            message: 'Image is required',
-          },
-        ]}
-      />
+    <Form
+      submitCallback={submitCallback}
+      error={error}
+    >
 
-      <Form.Input
-        name="username"
-        placeholder="Username"
-        inputType="text"
-        initialValue={initialState.username}
-        validators={[
-          {
-            required: true,
-            message: 'Username is required',
-          },
-          {
-            minLength: VALIDATIONS.USER.USERNAME_MIN_LENGTH,
-            message: `Username must be at least ${VALIDATIONS.USER.USERNAME_MIN_LENGTH} characters long`,
-          },
-        ]}
-      />
+      <div className={style['file-wrapper']}>
+        <Form.Input
+          name="image"
+          userProfileImage
+          initialValue={initialState.image}
+          inputType="file"
+        />
+      </div>
 
-      <Form.Input
-        name="email"
-        placeholder="Email"
-        inputType="text"
-        initialValue={initialState.email}
-        validators={[
-          {
-            required: true,
-            message: 'Email is required',
-          },
-          {
-            email: true,
-            message: 'Email address is invalid',
-          },
-        ]}
-      />
+      <div className={style.wrapper}>
+        <Form.Input
+          name="username"
+          placeholder="Username"
+          label="Username"
+          inputType="text"
+          initialValue={initialState.username}
+          validators={[
+            {
+              required: true,
+              message: 'Username is required',
+            },
+            {
+              minLength: VALIDATIONS.USER.USERNAME_MIN_LENGTH,
+              message: `Username must be at least ${VALIDATIONS.USER.USERNAME_MIN_LENGTH} characters long`,
+            },
+          ]}
+        />
 
-      <Form.Input
-        name="firstName"
-        placeholder="First Name"
-        inputType="text"
-        initialValue={initialState.firstName}
-        validators={[
-          {
-            required: true,
-            message: 'First Name is required',
-          },
-          {
-            minLength: VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH,
-            message: `First Name must be at least ${VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH} characters long`,
-          },
-        ]}
-      />
+        <Form.Input
+          name="email"
+          placeholder="Email"
+          label="Email"
+          inputType="text"
+          initialValue={initialState.email}
+          validators={[
+            {
+              required: true,
+              message: 'Email is required',
+            },
+            {
+              email: true,
+              message: 'Email address is invalid',
+            },
+          ]}
+        />
+      </div>
 
-      <Form.Input
-        name="lastName"
-        placeholder="Last Name"
-        inputType="text"
-        initialValue={initialState.lastName}
-        validators={[
-          {
-            required: true,
-            message: 'Last Name is required',
-          },
-          {
-            minLength: VALIDATIONS.USER.LAST_NAME_MIN_LENGTH,
-            message: `Last Name must be at least ${VALIDATIONS.USER.LAST_NAME_MIN_LENGTH} characters long`,
-          },
-        ]}
-      />
+      <div className={style.wrapper}>
+        <Form.Input
+          name="firstName"
+          placeholder="First Name"
+          label="First name"
+          inputType="text"
+          initialValue={initialState.firstName}
+          validators={[
+            {
+              required: true,
+              message: 'First Name is required',
+            },
+            {
+              minLength: VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH,
+              message: `First Name must be at least ${VALIDATIONS.USER.FIRST_NAME_MIN_LENGTH} characters long`,
+            },
+          ]}
+        />
+
+        <Form.Input
+          name="lastName"
+          placeholder="Last Name"
+          label="Last name"
+          inputType="text"
+          initialValue={initialState.lastName}
+          validators={[
+            {
+              required: true,
+              message: 'Last Name is required',
+            },
+            {
+              minLength: VALIDATIONS.USER.LAST_NAME_MIN_LENGTH,
+              message: `Last Name must be at least ${VALIDATIONS.USER.LAST_NAME_MIN_LENGTH} characters long`,
+            },
+          ]}
+        />
+      </div>
+
       <Button
         submitButton
         caption="Update"
