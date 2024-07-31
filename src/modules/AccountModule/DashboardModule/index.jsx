6251c76f62e198482dc68751d3829646f17ac903
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:24:18 +0300

    refactor add greeting message and removed old content

--- src/modules/AccountModule/DashboardModule/index.jsx
+++ src/modules/AccountModule/DashboardModule/index.jsx
@@ -1,41 +1,11 @@
-import { useSelector } from 'react-redux';
-import UserProfileImage from '../../../components/UserProfileImage';
-import { normalizeName } from '../../../helpers';
-
 import style from './index.module.css';
-import { selectUser } from '../../../store/auth/selectors';
 
 export default function DashboardModule() {
-  const user = useSelector(selectUser);
-
   return (
-    <div className={style['dashboard-container']}>
-      <UserProfileImage imageUrl={user.imageUrl} />
-
-      <div>
-        <div className={style.wrapper}>
-          <p>Name</p>
-          <p>
-            {normalizeName(user.firstName)}
-            &nbsp;
-            {normalizeName(user.lastName)}
-          </p>
-        </div>
-
-        <div className={style.wrapper}>
-          <p>Username</p>
-          <p>
-            {user.username}
-          </p>
-        </div>
-
-        <div className={style.wrapper}>
-          <p>Email</p>
-          <p>
-            {user.email}
-          </p>
-        </div>
-      </div>
-    </div>
+    <h4 className={style.greeting}>
+      Welcome to your travel hub! Manage your journeys, track your adventures,
+      and share your experiences all in one
+      place.
+    </h4>
   );
 }
