5aabfef47ee1d5e09236a98b626e37662209de70
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:14:48 +0300

    add app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
-import DefaultLayout from './layouts/DefaultLayout';
 import Navigation from './components/Navigation';
 import Loading from './components/Loading';
 import Footer from './components/Footer';
 import useDestinations from './hooks/useDestinations';
 import OptimisticModule from './modules/OptimisticModule';
+import AppLayout from './layouts/AppLayout';
 
 export default function App() {
   const { success: destinationsLoaded, fetchDestinations } = useDestinations();
@@ -15,16 +15,20 @@ export default function App() {
   }, []);
 
   if (!destinationsLoaded) {
-    return <Loading />;
+    return <AppLayout content={<Loading />} />;
   }
 
   return (
-    <DefaultLayout>
-      <Navigation />
-      <Outlet />
-      <Footer />
-      <OptimisticModule />
-      <ScrollRestoration />
-    </DefaultLayout>
+    <AppLayout
+      navigation={<Navigation />}
+      content={(
+        <>
+          <Outlet />
+          <OptimisticModule />
+          <ScrollRestoration />
+        </>
+      )}
+      footer={<Footer />}
+    />
   );
 }
