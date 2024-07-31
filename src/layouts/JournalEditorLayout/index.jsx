6a44a332d268d2eb0a5e22cc288782d42ae638ae
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:23:22 +0300

    refactor container style

--- src/layouts/JournalEditorLayout/index.jsx
+++ src/layouts/JournalEditorLayout/index.jsx
@@ -3,7 +3,7 @@ import Container from '../../components/Container';
 
 export default function JournalEditorLayout({ title, caption, children }) {
   return (
-    <Container customStyle={{ paddingTop: '5em' }}>
+    <Container customStyle={{ margin: '2em 0' }}>
       <InfoBlock
         title={title}
         caption={caption}
