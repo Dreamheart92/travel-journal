8be24a88d9f4f9d55d6f19bf0a61ff124db9ba5b
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:17:47 +0300

    removed handleLogout

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,24 +1,12 @@
-import { useNavigate } from 'react-router-dom';
 import NavigationLayout from '../../layouts/NavigationLayout';
 import Logo from './Logo';
 import NavigationMenu from './NavigationMenu';
-import { PATHS } from '../../constants/paths';
-import useAuth from '../../hooks/useAuth';
 
 export default function Navigation() {
-  const navigate = useNavigate();
-  const auth = useAuth();
-
-  const handleLogout = (event) => {
-    event.preventDefault();
-    auth.clearUser();
-    navigate(PATHS.HOME);
-  };
-
   return (
     <NavigationLayout>
       <Logo />
-      <NavigationMenu onLogout={handleLogout} />
+      <NavigationMenu />
     </NavigationLayout>
   );
 }
