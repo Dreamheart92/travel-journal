6fd1cc4c5296ecff13f7a7e67fa38eabe248c812
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:32:30 +0300

    add reseting journal state on unmount

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Container from '../../components/Container';
 import Hero from '../../components/Hero';
 import DestinationsModule from '../../modules/HomeModule/DestinationsModule';
 import LatestJournalsModule from '../../modules/HomeModule/LatestJournalsModule';
+import { entriesActions } from '../../store/entries';
+import entriesKeys from '../../store/entries/types';
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => () => dispatch(entriesActions.resetState({ key: entriesKeys.JOURNAL_ENTRIES })));
+
   return (
     <Container customStyle={{ width: '100%' }}>
       <Hero />
