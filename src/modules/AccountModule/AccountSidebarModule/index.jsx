ffa89f22b4bdd3d9689b00abebf1bf0cb9f40033
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:23:50 +0300

    refactor rendering user + navigation menu

--- src/modules/AccountModule/AccountSidebarModule/index.jsx
+++ src/modules/AccountModule/AccountSidebarModule/index.jsx
@@ -1,27 +1,47 @@
-import SidebarSection from '../../../components/Sidebar/SidebarSection';
+import { useSelector } from 'react-redux';
 import NavigationLink from '../../../components/NavigationLink';
-import { ACCOUNT_SUB_PATHS, PATHS } from '../../../constants/paths';
+import { ACCOUNT_SUB_PATHS } from '../../../constants/paths';
 import Sidebar from '../../../components/Sidebar';
+import UserProfileImage from '../../../components/UserProfileImage';
+import { selectUser } from '../../../store/auth/selectors';
+import style from './index.module.css';
+import useAuth from '../../../hooks/useAuth';
 
 export default function AccountSidebarModule() {
+  const user = useSelector(selectUser);
+  const auth = useAuth();
+
   return (
     <Sidebar width="fit-content">
-      <SidebarSection heading="My Account">
+      <div className={style.container}>
+        <div className={style.user}>
+          <UserProfileImage imageUrl={user.imageUrl} />
 
-        <NavigationLink to={PATHS.ACCOUNT} end>
-          My Account
-        </NavigationLink>
+          <div>
+            <p>{user.username}</p>
+          </div>
 
-        <NavigationLink to={ACCOUNT_SUB_PATHS.EDIT_PROFILE}>
-          Edit Profile
-        </NavigationLink>
-      </SidebarSection>
+          <p>{user.email}</p>
+        </div>
 
-      <SidebarSection heading="Journals">
-        <NavigationLink to={ACCOUNT_SUB_PATHS.MY_JOURNALS}>
-          My Journals
-        </NavigationLink>
-      </SidebarSection>
+        <div className={style.navigation}>
+          <NavigationLink account to={ACCOUNT_SUB_PATHS.EDIT_PROFILE}>
+            Edit Profile
+          </NavigationLink>
+
+          <NavigationLink account to={ACCOUNT_SUB_PATHS.MY_JOURNALS}>
+            My Journals
+          </NavigationLink>
+
+          <NavigationLink
+            onClick={auth.logout}
+            account
+            to=""
+          >
+            Sign out
+          </NavigationLink>
+        </div>
+      </div>
     </Sidebar>
   );
 }
