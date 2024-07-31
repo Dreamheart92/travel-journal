e62674f8aa2977024e8285038e536fa953945c1c
Toni Rachev <tonirachev92@gmail.com>
2024-07-31 23:18:25 +0300

    refactor using useAuth hook for logout

--- src/components/Navigation/NavigationMenu/UserNavigation/index.jsx
+++ src/components/Navigation/NavigationMenu/UserNavigation/index.jsx
@@ -1,12 +1,15 @@
 import NavigationLink from '../../../NavigationLink';
 import { PATHS } from '../../../../constants/paths';
+import useAuth from '../../../../hooks/useAuth';
 
-export default function UserNavigation({ onLogout }) {
+export default function UserNavigation() {
+  const auth = useAuth();
+
   return (
     <>
       <NavigationLink to={PATHS.CREATE}>Create</NavigationLink>
       <NavigationLink to={PATHS.ACCOUNT}>Account</NavigationLink>
-      <NavigationLink onClick={onLogout} to="">Logout</NavigationLink>
+      <NavigationLink onClick={auth.logout} to="">Logout</NavigationLink>
     </>
   );
 }
