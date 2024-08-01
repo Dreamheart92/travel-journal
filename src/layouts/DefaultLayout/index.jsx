589b32282de67ead5e03a1c8731933c07544118c
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 23:46:17 +0300

    deleted default layout

--- src/layouts/DefaultLayout/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from './index.module.css';
-
-export default function DefaultLayout({ children }) {
-  return (
-    <div className={style.container}>
-      {children}
-    </div>
-  );
-}
