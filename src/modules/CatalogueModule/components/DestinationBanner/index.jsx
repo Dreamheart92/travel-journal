33f1f223c49b15ff3048604c365951501fb6c824
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 23:18:01 +0300

    renamed to destination banner

---
+++ src/modules/CatalogueModule/components/DestinationBanner/index.jsx
@@ -0,0 +1,20 @@
+import Image from '../../../../components/Image';
+import style from './index.module.css';
+import { findCurrentDestination } from '../../../../helpers/helpers';
+import useDestinations from '../../../../hooks/useDestinations';
+
+export default function DestinationBanner({ destination }) {
+  const { destinations } = useDestinations();
+
+  const currentDestination = findCurrentDestination(destination, destinations);
+
+  const imageUrl = currentDestination ? currentDestination.imageUrl : '/around-the-world.jpg';
+  const caption = currentDestination ? currentDestination.name : 'Around the World';
+
+  return (
+    <div className={style['destination-header']}>
+      <Image imageUrl={imageUrl} />
+      <h1 className={style.caption}>{caption}</h1>
+    </div>
+  );
+}
