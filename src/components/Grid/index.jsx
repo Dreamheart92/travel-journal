427d6b16ffaa141634547c96ae15b81b9294a68c
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 22:45:04 +0300

    removed outerClassName prop

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,8 +1,8 @@
 import style from './index.module.css';
 
-export default function Grid({ outerClassName = '', children }) {
+export default function Grid({ children }) {
   return (
-    <div className={`${style.grid} ${outerClassName}`}>
+    <div className={style.grid}>
       {children}
     </div>
   );
