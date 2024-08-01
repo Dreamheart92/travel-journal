bd1cb139ea1cf02860ef0f1cdc9e8e98a8876a0d
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 13:39:10 +0300

    add size prop

--- src/components/UserProfileImage/index.jsx
+++ src/components/UserProfileImage/index.jsx
@@ -1,11 +1,13 @@
 import Image from '../Image';
 import style from './index.module.css';
 
-export default function UserProfileImage({ imageUrl }) {
+export default function UserProfileImage({ imageUrl, size = 'md' }) {
   const image = imageUrl || '/userPlaceholder.png';
 
+  const wrapperClassName = size === 'md' ? style.medium : style.small;
+
   return (
-    <div className={style['profile-image']}>
+    <div className={wrapperClassName}>
       <Image imageUrl={image} />
     </div>
   );
