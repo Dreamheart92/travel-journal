bd24bd3824c0d428e163dfddf128d920d0b7d527
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 13:40:32 +0300

    refactor replaced entries with journals reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import destinationsReducer from './destinations';
 import crudReducer from './crud';
-import entriesReducer from './entries';
 import authReducer from './auth';
 import searchReducer from './search';
 import optimisticReducer from './optimistic';
+import journalsReducer from './journals';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     destinations: destinationsReducer,
     crud: crudReducer,
-    entries: entriesReducer,
+    journals: journalsReducer,
     search: searchReducer,
     optimistic: optimisticReducer,
   },
