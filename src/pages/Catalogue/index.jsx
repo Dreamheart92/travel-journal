6af9dfea6b472b8af005891259984e08c46697bf
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 23:18:49 +0300

    removed isAuthenticated and currentDestination

--- src/pages/Catalogue/index.jsx
+++ src/pages/Catalogue/index.jsx
@@ -1,37 +1,28 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import DestinationHeader from '../../modules/CatalogueModule/components/DestinationHeader';
 import style from './index.module.css';
 import useQuery from '../../hooks/useQuery';
-import { selectIsAuthenticated } from '../../store/auth/selectors';
-import useDestinations from '../../hooks/useDestinations';
 import CatalogueListModule from '../../modules/CatalogueModule/CatalogueListModule';
 import CatalogueSidebarModule from '../../modules/CatalogueModule/CatalogueSidebarModule';
 import { journalsActions } from '../../store/journals';
 import { JOURNALS_STATE_KEYS } from '../../constants/redux';
 import Container from '../../components/Container';
+import DestinationBanner from '../../modules/CatalogueModule/components/DestinationBanner';
 
 export default function Catalogue() {
   const dispatch = useDispatch();
 
   const { destination } = useParams();
-  const { destinations } = useDestinations();
 
   const { searchParams, onQuery } = useQuery();
-
-  const isAuthenticated = useSelector(selectIsAuthenticated);
-
-  const currentDestination = destination
-    ? destinations.find((destinationFilter) => destinationFilter.name === destination)
-    : null;
 
   useEffect(() => () => dispatch(journalsActions.resetState({ key: JOURNALS_STATE_KEYS.JOURNALS })));
 
   return (
     <Container customStyle={{ paddingBottom: '5em' }}>
 
-      <DestinationHeader destination={currentDestination} />
+      <DestinationBanner destination={destination} />
 
       <div className={style.wrapper}>
 
@@ -42,7 +33,6 @@ export default function Catalogue() {
         />
 
         <CatalogueSidebarModule
-          isAuthenticated={isAuthenticated}
           onQuery={onQuery}
         />
 
