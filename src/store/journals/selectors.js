d8f67a14a2de36b6b9c27d8e1e041e0e6ac89492
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 16:33:35 +0300

    refactor renamed to selectJournalsSlice

--- src/store/journals/selectors.js
+++ src/store/journals/selectors.js
@@ -1,14 +1,14 @@
 import { createSelector } from '@reduxjs/toolkit';
 import entriesKeys from './types';
 
-const selectEntriesSlice = (state) => state.journals;
+const selectJournalsSlice = (state) => state.journals;
 
-export const selectJournalsEntries = (state) => state.journals[entriesKeys.JOURNAL_ENTRIES];
+export const selectJournals = (state) => state.journals[entriesKeys.JOURNAL_ENTRIES];
 
-export const selectJournalEntry = (state) => state.journals[entriesKeys.JOURNAL_ENTRY];
+export const selectJournal = (state) => state.journals[entriesKeys.JOURNAL_ENTRY];
 
 export const selectComments = createSelector(
-  [selectEntriesSlice, selectJournalsEntries],
+  [selectJournalsSlice, selectJournals],
   (journalsSlice, journalEntries) => ({
     comments: journalsSlice[entriesKeys.COMMENTS],
     loading: journalEntries.loading,
