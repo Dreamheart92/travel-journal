98047cb3c962c6b0ba65298e3b189761cf1ae80b
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 23:17:38 +0300

    add isAuthenthicated selector

--- src/modules/CatalogueModule/CatalogueSidebarModule/index.jsx
+++ src/modules/CatalogueModule/CatalogueSidebarModule/index.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
 import Sidebar from '../../../components/Sidebar';
 import CreateJournal from '../components/CreateJournal';
 import Search from '../../../components/Search';
 import FiltersSection from '../components/FiltersSection';
+import { selectIsAuthenticated } from '../../../store/auth/selectors';
+
+export default function CatalogueSidebarModule({ onQuery }) {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
 
   return (
     <Sidebar width="20em">
