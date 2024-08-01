23eef9fb9ad5752e1260ad86ddfbe2409b291b08
Toni Rachev <tonirachev92@gmail.com>
2024-08-01 17:10:27 +0300

    add OPTIMISTIC_STATE_KEYS

--- src/constants/redux.js
+++ src/constants/redux.js
@@ -11,3 +11,10 @@ const JOURNALS_STATE_KEYS = {
   COMMENTS: 'comments',
 };
 
+const OPTIMISTIC_STATE_KEYS = {
+  POST_COMMENT: 'postComment',
+  DELETE_COMMENT: 'deleteComment',
+  POST_COMMENT_REACTION: 'postCommentReaction',
+};
+
+export { CRUD_STATE_KEYS, JOURNALS_STATE_KEYS, OPTIMISTIC_STATE_KEYS };
