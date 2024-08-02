72ea9513f060cf44dcce436be1d25e39ddf2cf9d
Toni Rachev <tonirachev92@gmail.com>
2024-08-02 11:21:36 +0300

    add lazy loading

--- src/router.jsx
+++ src/router.jsx
@@ -1,23 +1,26 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import { PATHS } from './constants/paths';
 
 import App from './App';
 import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Catalogue from './pages/Catalogue';
-import Details from './pages/Details';
-import Create from './pages/Create';
-import Edit from './pages/Edit';
 import Account from './pages/Account';
-import DashboardModule from './modules/AccountModule/DashboardModule';
-import EditProfileModule from './modules/AccountModule/EditProfileModule';
-import MyJournalsModule from './modules/AccountModule/MyJournalsModule';
 
 import AuthGuard from './guards/AuthGuard';
 import JournalOwnerGuard from './guards/JournalOwnerGuard';
 import RestrictAuthenticatedGuard from './guards/RestrictAuthenticatedGuard';
 import NotFound from './pages/NotFound';
+import Loading from './components/Loading';
+
+const Login = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Catalogue = lazy(() => import('./pages/Catalogue'));
+const Details = lazy(() => import('./pages/Details'));
+const Create = lazy(() => import('./pages/Create'));
+const Edit = lazy(() => import('./pages/Edit'));
+const DashboardModule = lazy(() => import('./modules/AccountModule/DashboardModule'));
+const EditProfileModule = lazy(() => import('./modules/AccountModule/EditProfileModule'));
+const MyJournalsModule = lazy(() => import('./modules/AccountModule/MyJournalsModule'));
 
 const router = createBrowserRouter([
   {
@@ -30,22 +33,30 @@ const router = createBrowserRouter([
       {
         path: PATHS.LOGIN,
         element: (
-          <RestrictAuthenticatedGuard>
-            <Login />
-          </RestrictAuthenticatedGuard>
+          <Suspense fallback={<div><Loading /></div>}>
+            <RestrictAuthenticatedGuard>
+              <Login />
+            </RestrictAuthenticatedGuard>
+          </Suspense>
         ),
       },
       {
         path: PATHS.SIGNUP,
         element: (
-          <RestrictAuthenticatedGuard>
-            <Signup />
-          </RestrictAuthenticatedGuard>
+          <Suspense fallback={<div><Loading /></div>}>
+            <RestrictAuthenticatedGuard>
+              <Signup />
+            </RestrictAuthenticatedGuard>
+          </Suspense>
         ),
       },
       {
         path: PATHS.CATALOGUE,
-        element: <Catalogue />,
+        element: (
+          <Suspense fallback={<div><Loading /></div>}>
+            <Catalogue />
+          </Suspense>
+        ),
       },
       {
         path: `${PATHS.CATALOGUE}/:destination`,
@@ -53,24 +64,32 @@ const router = createBrowserRouter([
       },
       {
         path: `${PATHS.DETAILS}/:journalId`,
-        element: <Details />,
+        element: (
+          <Suspense fallback={<div><Loading /></div>}>
+            <Details />
+          </Suspense>
+        ),
       },
       {
         path: PATHS.CREATE,
         element: (
-          <AuthGuard>
-            <Create />
-          </AuthGuard>
+          <Suspense fallback={<div><Loading /></div>}>
+            <AuthGuard>
+              <Create />
+            </AuthGuard>
+          </Suspense>
         ),
       },
       {
         path: `${PATHS.EDIT}/:journalId`,
         element: (
-          <AuthGuard>
-            <JournalOwnerGuard>
-              <Edit />
-            </JournalOwnerGuard>
-          </AuthGuard>
+          <Suspense fallback={<div><Loading /></div>}>
+            <AuthGuard>
+              <JournalOwnerGuard>
+                <Edit />
+              </JournalOwnerGuard>
+            </AuthGuard>
+          </Suspense>
         ),
       },
       {
@@ -83,15 +102,27 @@ const router = createBrowserRouter([
         children: [
           {
             path: '',
-            element: <DashboardModule />,
+            element: (
+              <Suspense fallback={<div><Loading /></div>}>
+                <DashboardModule />
+              </Suspense>
+            ),
           },
           {
             path: 'edit-profile',
-            element: <EditProfileModule />,
+            element: (
+              <Suspense fallback={<div><Loading /></div>}>
+                <EditProfileModule />
+              </Suspense>
+            ),
           },
           {
             path: 'my-journals',
-            element: <MyJournalsModule />,
+            element: (
+              <Suspense fallback={<div><Loading /></div>}>
+                <MyJournalsModule />
+              </Suspense>
+            ),
           },
         ],
       },
