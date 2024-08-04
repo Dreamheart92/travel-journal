ac63fe8165001fb5449e9824f565d85bbdede829
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 19:51:27 +0300

    renamed to ReadMoreButton

--- src/components/JournalCards/CatalogueCard/ReadMoreButton/index.jsx
+++ src/components/JournalCards/CatalogueCard/ReadMoreButton/index.jsx
@@ -2,17 +2,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import style from './index.module.css';
 
-export default function CatalogueCardButton({ caption }) {
+export default function ReadMoreButton() {
   return (
     <button
       className={style.button}
       type="button"
     >
       <span className={style.visible}>
-        {caption}
+        READ MORE
       </span>
       <FontAwesomeIcon className={style['arrow-icon']} icon={faArrowRight} />
-      <span className={style.hidden}>{caption}</span>
+      <span className={style.hidden}>READ MORE</span>
     </button>
   );
 }
