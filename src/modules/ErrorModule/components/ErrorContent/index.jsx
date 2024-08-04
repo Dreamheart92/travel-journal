0189467e6466f8f0cab96d601c8d680d821052ad
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:27:18 +0300

    created error module

---
+++ src/modules/ErrorModule/components/ErrorContent/index.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '../../../../constants/paths';
+import InfoBlock from '../../../../components/InfoBlock';
+import { ERROR_PAGE_MESSAGE, ERROR_PAGE_TYPES } from '../../../../constants/errorMessages';
+import Button from '../../../../components/Button';
+
+export default function ErrorContent({ notFoundError }) {
+  return (
+    <>
+    </>
+  );
+}
