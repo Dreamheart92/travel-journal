149a8f9ec7a418d467366f76cc01fa6e84837ebf
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 19:54:21 +0300

    renamed to CardImage

--- src/components/JournalCards/HomeCard/CardImage/index.jsx
+++ src/components/JournalCards/HomeCard/CardImage/index.jsx
@@ -1,7 +1,7 @@
-import Image from '../../Image';
+import Image from '../../../Image';
 import style from './index.module.css';
 
-export default function JournalCardImage({ imageUrl, size }) {
+export default function CardImage({ imageUrl, size }) {
   return (
     <div style={size} className={style['journal-image']}>
       <Image imageUrl={imageUrl} />
