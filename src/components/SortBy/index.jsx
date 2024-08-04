3039cf26f2590f522ac978b8a0842a586d163707
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:22:37 +0300

    consuming useSearch and add handleSelect

--- src/components/SortBy/index.jsx
+++ src/components/SortBy/index.jsx
@@ -3,5 +3,14 @@ import style from './index.module.css';
 import SidebarSection from '../Sidebar/SidebarSection';
 
 export default function SortBy({ onQuery }) {
+  const { sortBy, sortByOptions, onSortBy } = useSearch();
+
+  const handleSelect = (event) => {
+    const { value } = event.target;
+    const selectedOptionId = event.target.selectedOptions[0].id;
+    onSortBy({ normalized_name: selectedOptionId, name: value });
+    onQuery('sortBy', value);
+  };
+
   );
 }
