b207f1761d1fa41f4ffa3940ec572a4a7ec8b203
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:33:41 +0300

    add sortByOptions

--- src/store/search/initialState.js
+++ src/store/search/initialState.js
@@ -4,6 +4,12 @@ const initialState = {
   totalPages: null,
   isSearching: false,
   sortBy: { normalized_name: 'Newest', name: 'newest' },
+  sortByOptions: [
+    { normalized_name: 'Newest', name: 'newest' },
+    { normalized_name: 'Oldest', name: 'oldest' },
+    { normalized_name: 'Most Commented', name: 'mostCommented' },
+    { normalized_name: 'Least Commented', name: 'leastCommented' },
+  ],
 };
 
 export default initialState;
