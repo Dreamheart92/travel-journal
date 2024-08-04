1abfc69d2c500d6ef4bf184548b529f69e47cce6
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:31:44 +0300

    created error module

---
+++ src/modules/ErrorModule/index.jsx
@@ -0,0 +1,8 @@
+import notFoundImage from '/not-found-background.jpg';
+import serverErrorImage from '/server-error-background.png';
+import Image from '../../components/Image';
+import ErrorContent from './components/ErrorContent';
+import style from './index.module.css';
+
+export default function ErrorModule({ notFoundPageError = false }) {
+}
