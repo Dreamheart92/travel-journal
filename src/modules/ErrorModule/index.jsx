2c575ef758b5fd04e516cf2de2de6983f13f7eee
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:32:12 +0300

    rendering content conditionally if notFoundPage or serverError

--- src/modules/ErrorModule/index.jsx
+++ src/modules/ErrorModule/index.jsx
@@ -5,4 +5,16 @@ import ErrorContent from './components/ErrorContent';
 import style from './index.module.css';
 
 export default function ErrorModule({ notFoundPageError = false }) {
+  const image = notFoundPageError ? notFoundImage : serverErrorImage;
+
+  return (
+    <div className={style.container}>
+      <div className={style.message}>
+        <ErrorContent notFoundError={notFoundPageError} />
+      </div>
+      <div className={style['image-wrapper']}>
+        <Image imageUrl={image} />
+      </div>
+    </div>
+  );
 }
