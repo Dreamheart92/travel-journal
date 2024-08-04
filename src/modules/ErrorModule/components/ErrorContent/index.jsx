872a68f03004aa6931561f4f3e8768da4868fc53
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:31:28 +0300

    implemented buttonAction and message

--- src/modules/ErrorModule/components/ErrorContent/index.jsx
+++ src/modules/ErrorModule/components/ErrorContent/index.jsx
@@ -5,8 +5,44 @@ import { ERROR_PAGE_MESSAGE, ERROR_PAGE_TYPES } from '../../../../constants/erro
 import Button from '../../../../components/Button';
 
 export default function ErrorContent({ notFoundError }) {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(PATHS.HOME);
+  };
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  const errorType = notFoundError
+    ? {
+      type: ERROR_PAGE_TYPES.NOT_FOUND,
+      buttonAction: handleRedirect,
+    }
+    : {
+      type: ERROR_PAGE_TYPES.SERVER_ERROR,
+      buttonAction: handleRefresh,
+    };
+
+  const {
+    code,
+    title,
+    caption,
+    buttonCaption,
+  } = ERROR_PAGE_MESSAGE[errorType.type];
+
   return (
     <>
+      <h1 style={{ fontWeight: 'bold' }}>{code}</h1>
+      <InfoBlock
+        title={title}
+        caption={caption}
+      />
+      <Button
+        onClick={errorType.buttonAction}
+        caption={buttonCaption}
+      />
     </>
   );
 }
