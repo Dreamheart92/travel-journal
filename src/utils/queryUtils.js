fff97880b5b8905e46a464566d4991bbb24ef90f
Toni Rachev <tonirachev92@gmail.com>
2024-08-05 00:01:32 +0300

    refactor buildQueryString

--- src/utils/queryUtils.js
+++ src/utils/queryUtils.js
@@ -1,9 +1,1 @@
-export const buildQueryString = (searchParams) => {
-  const queries = [];
-
-  searchParams.entries().forEach(([searchKey, searchValue]) => {
-    queries.push(`${searchKey}=${searchValue}`);
-  });
-
-  return queries.length > 0 ? `?${queries.join('&')}` : '';
-};
+export const buildQueryString = (searchParams) => `?${searchParams.toString()}`;
