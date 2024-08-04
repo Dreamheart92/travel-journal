8bc1af139c816e7b86bbbac6858047abe940b343
Toni Rachev <tonirachev92@gmail.com>
2024-08-05 00:50:26 +0300

    implemented handleRedirect

--- src/components/JournalCards/CatalogueCard/ReadMoreButton/index.jsx
+++ src/components/JournalCards/CatalogueCard/ReadMoreButton/index.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import style from './index.module.css';
+import { PATHS } from '../../../../constants/paths';
 
-export default function ReadMoreButton() {
+export default function ReadMoreButton({ journalId }) {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(`${PATHS.DETAILS}/${journalId}`);
+  };
+
   return (
     <button
       className={style.button}
       type="button"
+      onClick={handleRedirect}
     >
       <span className={style.visible}>
         READ MORE
