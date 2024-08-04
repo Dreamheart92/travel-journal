756619502488042551641ef8c0dd7a3111850461
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:23:22 +0300

    add ERROR_PAGE_MESSAGE

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -11,8 +11,23 @@ const ERROR_PAGE_TYPES = {
   SERVER_ERROR: 'SERVER_ERROR',
 };
 
+const ERROR_PAGE_MESSAGE = {
+  [ERROR_PAGE_TYPES.NOT_FOUND]: {
+    code: '404',
+    title: 'Oops! Looks like we’ve drifted off course.',
+    caption: 'Let’s navigate back to safety.',
+    buttonCaption: 'Back to home',
+  },
+  [ERROR_PAGE_TYPES.SERVER_ERROR]: {
+    code: '500',
+    title: 'Oops! Something Went Wrong',
+    caption: 'We’re experiencing some technical difficulties and are working hard to fix the issue. Please refresh or try again later.',
+    buttonCaption: 'Refresh',
+  },
+};
 
 export {
   optimisticErrorMessages,
+  ERROR_PAGE_MESSAGE,
   ERROR_PAGE_TYPES,
 };
