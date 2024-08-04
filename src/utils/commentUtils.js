8fec8e625964b249aca915837d83c490b2dc6571
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 16:52:02 +0300

    moved comment helpers to commentUtils

---
+++ src/utils/commentUtils.js
@@ -0,0 +1,11 @@
+const buildTemporaryCommentId = () => `${Math.random() * 1000}___temporary`;
+
+export const buildLocalComment = (user, comment) => ({
+  _id: buildTemporaryCommentId(),
+  createdAt: String(new Date()),
+  author: user,
+  comment,
+  dislikes: [],
+  likes: [],
+  totalLikes: 0,
+});
