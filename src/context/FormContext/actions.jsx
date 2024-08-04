84166e8d846fb893b3025f15a70cc02a001ed382
Toni Rachev <tonirachev92@gmail.com>
2024-08-04 11:23:46 +0300

    refactor contextActions

--- src/context/FormContext/actions.jsx
+++ src/context/FormContext/actions.jsx
@@ -1,23 +1,21 @@
 import * as actionTypes from './types';
 
-const contextActions = (dispatch) => {
-  return {
-    initFormState: (initialState) => {
-      dispatch({ type: actionTypes.INIT_FORM_STATE, payload: { initialState } });
-    },
-    onChange: (event) => {
-      dispatch({ type: actionTypes.HANDLE_CHANGE, payload: { event } });
-    },
-    onBlur: (event) => {
-      dispatch({ type: actionTypes.HANDLE_BLUR, payload: { event } });
-    },
-    resetField: (fieldName) => {
-      dispatch({ type: actionTypes.RESET_FIELD, payload: { fieldName } });
-    },
-    onSubmit: () => {
-      dispatch({ type: actionTypes.HANDLE_SUBMIT });
-    },
-  };
-};
+const contextActions = (dispatch) => ({
+  initFormState: (initialState) => {
+    dispatch({ type: actionTypes.INIT_FORM_STATE, payload: { initialState } });
+  },
+  onChange: (event) => {
+    dispatch({ type: actionTypes.HANDLE_CHANGE, payload: { event } });
+  },
+  onBlur: (event) => {
+    dispatch({ type: actionTypes.HANDLE_BLUR, payload: { event } });
+  },
+  resetField: (fieldName) => {
+    dispatch({ type: actionTypes.RESET_FIELD, payload: { fieldName } });
+  },
+  onSubmit: () => {
+    dispatch({ type: actionTypes.HANDLE_SUBMIT });
+  },
+});
 
 export default contextActions;
