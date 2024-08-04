f1c9f46010f8ac7f7845b5230f375739dba76579
Toni Rachev <tonirachev92@gmail.com>
2024-08-05 00:51:54 +0300

    add link to destination catalogue

--- src/components/JournalDetails/DestinationDetails/index.jsx
+++ src/components/JournalDetails/DestinationDetails/index.jsx
@@ -1,13 +1,19 @@
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons/faGlobe';
 import JournalDetailsContainer from '../JournalDetailsContainer';
 import { normalizeName } from '../../../utils/stringUtils';
+import { PATHS } from '../../../constants/paths';
 
 export default function DestinationDetails({ destination }) {
   return (
     <JournalDetailsContainer>
-      <FontAwesomeIcon icon={faGlobe} />
-      <p>{normalizeName(destination)}</p>
+      <Link
+        to={`${PATHS.CATALOGUE}/${destination}`}
+      >
+        <FontAwesomeIcon icon={faGlobe} />
+        <p>{normalizeName(destination)}</p>
+      </Link>
     </JournalDetailsContainer>
   );
 }
