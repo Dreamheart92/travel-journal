8d24529c08e48844db99fe6f05cf9e97c7290c0e
Toni Rachev <tonirachev92@gmail.com>
2024-08-05 12:25:58 +0300

    refactor receive author prop and add link to catalogue page

--- src/components/JournalDetails/AuthorDetails/index.jsx
+++ src/components/JournalDetails/AuthorDetails/index.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
 import UserProfileImage from '../../UserProfileImage';
 import JournalDetailsContainer from '../JournalDetailsContainer';
+import { PATHS } from '../../../constants/paths';
 
-export default function AuthorDetails({ image, username }) {
+export default function AuthorDetails({ author }) {
   return (
     <JournalDetailsContainer>
-      <UserProfileImage size="sm" imageUrl={image} />
-      <p>{username}</p>
+      <Link to={`${PATHS.CATALOGUE}?author=${author._id}`}>
+        <UserProfileImage size="sm" imageUrl={author.imageUrl} />
+        <p>{author.username}</p>
+      </Link>
     </JournalDetailsContainer>
   );
 }
