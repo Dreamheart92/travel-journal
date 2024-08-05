70193d2ef19cdb93dc5919f3055468216ef6ba14
Toni Rachev <tonirachev92@gmail.com>
2024-08-05 16:15:20 +0300

    add like_journal state

--- src/constants/redux.js
+++ src/constants/redux.js
@@ -15,6 +15,7 @@ const OPTIMISTIC_STATE_KEYS = {
   POST_COMMENT: 'postComment',
   DELETE_COMMENT: 'deleteComment',
   POST_COMMENT_REACTION: 'postCommentReaction',
+  LIKE_JOURNAL: 'likeJournal',
 };
 
 export { CRUD_STATE_KEYS, JOURNALS_STATE_KEYS, OPTIMISTIC_STATE_KEYS };
