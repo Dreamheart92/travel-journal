85e464155ba91b8ac4912e71fb5852cc1c854043
Toni Rachev <tonirachev92@gmail.com>
2024-08-06 17:20:24 +0300

    refactor selectors

--- src/store/journals/selectors.js
+++ src/store/journals/selectors.js
@@ -1,16 +1,13 @@
-import { createSelector } from '@reduxjs/toolkit';
 import { JOURNALS_STATE_KEYS } from '../../constants/redux';
 
 const selectJournalsSlice = (state) => state.journals;
 
-export const selectJournals = (state) => state.journals[JOURNALS_STATE_KEYS.JOURNALS];
-
 export const selectJournal = (state) => state.journals[JOURNALS_STATE_KEYS.JOURNAL];
 
-export const selectComments = createSelector(
-  [selectJournalsSlice, selectJournals],
-  (journalsSlice, journals) => ({
-    comments: journalsSlice[JOURNALS_STATE_KEYS.COMMENTS],
-    loading: journals.loading,
-  }),
-);
+export const selectComments = (state) => state.journals[JOURNALS_STATE_KEYS.COMMENTS];
+
+export const selectHomeJournals = (state) => state.journals[JOURNALS_STATE_KEYS.HOME];
+
+export const selectCatalogue = (state) => state.journals[JOURNALS_STATE_KEYS.CATALOGUE];
+
+export const selectUserJournals = (state) => state.journals[JOURNALS_STATE_KEYS.USER_JOURNALS];
