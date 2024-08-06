4e31c7338e596c1def2c856ab64d713d123841cf
Toni Rachev <tonirachev92@gmail.com>
2024-08-06 17:18:50 +0300

    refactor removed resetting journals state on unmount

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,14 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import Container from '../../components/Container';
 import Hero from '../../components/Hero';
 import DestinationsModule from '../../modules/HomeModule/DestinationsModule';
-import LatestJournalsModule from '../../modules/HomeModule/LatestJournalsModule';
-import { journalsActions } from '../../store/journals';
-import { JOURNALS_STATE_KEYS } from '../../constants/redux';
+import TopJournalsModule from '../../modules/HomeModule/TopJournalsModule';
 
 export default function Home() {
-  const dispatch = useDispatch();
-
-  useEffect(() => () => dispatch(journalsActions.resetState({ key: JOURNALS_STATE_KEYS.JOURNALS })));
-
   return (
     <Container customStyle={{ width: '100%' }}>
       <Hero />
       <DestinationsModule />
-      <LatestJournalsModule />
+      <TopJournalsModule />
     </Container>
   );
 }
