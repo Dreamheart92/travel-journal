b22fa2e36e52332f5ed3b203a0cb8474a751a805
Toni Rachev <tonirachev92@gmail.com>
2024-08-06 17:19:42 +0300

    refactor use dynamic key for state management

--- src/store/journals/extraReducers.js
+++ src/store/journals/extraReducers.js
@@ -1,53 +1,36 @@
-import { fetchJournalService } from './services';
-import { JOURNALS_STATE_KEYS } from '../../constants/redux';
 import { buildErrorObject } from '../../utils/errorUtils';
+import { JOURNALS_STATE_KEYS } from '../../constants/redux';
 
 const extraReducers = (builder) => {
-  builder.addCase(fetchJournalService.pending, (state) => {
-    state[JOURNALS_STATE_KEYS.JOURNAL].loading = true;
-    state[JOURNALS_STATE_KEYS.JOURNAL].success = false;
-    state[JOURNALS_STATE_KEYS.JOURNAL].error = null;
-  });
-  builder.addCase(fetchJournalService.fulfilled, (state, action) => {
-    state[JOURNALS_STATE_KEYS.JOURNAL].result = action.payload.result;
-    state[JOURNALS_STATE_KEYS.JOURNAL].result.isJournalOwner = action.payload.isJournalOwner;
-    state[JOURNALS_STATE_KEYS.COMMENTS].results = action.payload.comments;
-
-    state[JOURNALS_STATE_KEYS.JOURNAL].loading = false;
-    state[JOURNALS_STATE_KEYS.JOURNAL].success = true;
-  });
-  builder.addCase(fetchJournalService.rejected, (state, action) => {
-    if (action.error.message !== 'Aborted') {
-      state[JOURNALS_STATE_KEYS.JOURNAL].loading = false;
-      state[JOURNALS_STATE_KEYS.JOURNAL].error = action.error;
-    }
-  });
   builder.addMatcher(
     (action) => action.type.startsWith('journals/fetchJournals'),
     (state, action) => {
       const { requestStatus } = action.meta;
+      const { key } = action.meta.arg;
 
       switch (requestStatus) {
         case 'pending': {
-          state[JOURNALS_STATE_KEYS.JOURNALS].loading = true;
-          state[JOURNALS_STATE_KEYS.JOURNALS].success = false;
-          state[JOURNALS_STATE_KEYS.JOURNALS].error = null;
+          state[key].loading = true;
+          state[key].success = false;
+          state[key].error = null;
           break;
         }
         case 'fulfilled': {
-          state[JOURNALS_STATE_KEYS.JOURNALS].results = {
-            journals: action.payload.journals,
-            totalPages: action.payload.totalPages,
-          };
+          if (key === JOURNALS_STATE_KEYS.JOURNAL) {
+            state[key].results = action.payload.journal;
+            state[JOURNALS_STATE_KEYS.COMMENTS] = action.payload.comments;
+          } else {
+            state[key].results = action.payload;
+          }
 
-          state[JOURNALS_STATE_KEYS.JOURNALS].loading = false;
-          state[JOURNALS_STATE_KEYS.JOURNALS].success = true;
+          state[key].loading = false;
+          state[key].success = true;
           break;
         }
         case 'rejected': {
           if (action.error.message !== 'Aborted') {
-            state[JOURNALS_STATE_KEYS.JOURNALS].error = buildErrorObject('Failed to fetch journals. Please try again.');
-            state[JOURNALS_STATE_KEYS.JOURNALS].loading = false;
+            state[key].error = buildErrorObject('Failed to fetch journals. Please try again.');
+            state[key].loading = false;
           }
           break;
         }
